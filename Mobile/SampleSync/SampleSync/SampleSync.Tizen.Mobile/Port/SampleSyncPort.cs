fb4a65d3c6e25e0563e44c9f92c7973c1784c2cf
Ickhee Woo <ickhee.woo@samsung.com>
2017-10-24 23:06:29 +0900

    uploaded SampleSync sample application
    Signed-off-by: Ickhee Woo <ickhee.woo@samsung.com>

---
+++ Mobile/SampleSync/SampleSync/SampleSync.Tizen.Mobile/Port/SampleSyncPort.cs
@@ -0,0 +1,368 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using TAS = Tizen.Account.SyncManager;
+using SampleSync.Models;
+using SampleSync.Utils;
+using System;
+using System.Collections.Generic;
+
+namespace SampleSync.Tizen.Port
+{
+    public class SampleSyncPort : ISyncAPIs
+    {
+        private TAS.SyncAdapter adapter;
+
+        static IPlatformEvent pEvent;
+
+		// int variables to store each sync job ides
+        private static int Periodic { get; set; }
+        private static int Calendar { get; set; }
+        private static int Contact { get; set; }
+        private static int Image { get; set; }
+        private static int Music { get; set; }
+        private static int Sound { get; set; }
+        private static int Video { get; set; }
+
+        public SampleSyncPort()
+        {
+        }
+
+        /// <summary>
+        /// Used to be invoked by Sync Service.
+        /// </summary>
+        /// <returns>true for success, other values for failure</returns>
+        static bool StartSyncCallback(TAS.SyncJobData request)
+        {
+            if (request.SyncJobName == "RequestOnDemand")
+            {
+                // Synchronize data with a server here
+				// Update when the OnDemand sync job is completed
+                pEvent.UpdateOnDemandDate(DateTime.Now.ToString());
+            }
+            else if (request.SyncJobName == "AddPeriodic")
+            {
+                // Synchronize data with a server here
+				// Update when the Periodic sync job is completed
+                pEvent.UpdatePeriodicDate(DateTime.Now.ToString());
+            }
+            else if (request.SyncJobName == TAS.SyncJobData.CalendarCapability)
+            {
+                // Synchronize data with a server here
+				// Update when the Calendar data change sync job is completed
+                pEvent.UpdateCalendarDate(DateTime.Now.ToString());
+            }
+            else if (request.SyncJobName == TAS.SyncJobData.ContactCapability)
+            {
+                // Synchronize data with a server here
+				// Update when the Contact data change sync job is completed
+                pEvent.UpdateContactDate(DateTime.Now.ToString());
+            }
+            else if (request.SyncJobName == TAS.SyncJobData.ImageCapability)
+            {
+                // Synchronize data with a server here
+				// Update when the Image data change sync job is completed
+                pEvent.UpdateImageDate(DateTime.Now.ToString());
+            }
+            else if (request.SyncJobName == TAS.SyncJobData.MusicCapability)
+            {
+                // Synchronize data with a server here
+				// Update when the Music data change sync job is completed
+                pEvent.UpdateMusicDate(DateTime.Now.ToString());
+            }
+            else if (request.SyncJobName == TAS.SyncJobData.SoundCapability)
+            {
+                // Synchronize data with a server here
+				// Update when the Sound data change sync job is completed
+                pEvent.UpdateSoundDate(DateTime.Now.ToString());
+            }
+            else if (request.SyncJobName == TAS.SyncJobData.VideoCapability)
+            {
+                // Synchronize data with a server here
+				// Update when the Video data change sync job is completed
+                pEvent.UpdateVideoDate(DateTime.Now.ToString());
+            }
+
+            return true;
+        }
+
+        public void CancelSyncCallback(TAS.SyncJobData data)
+        {
+        }
+
+        /// <summary>
+        /// Used to gain sync job ides for registered sync jobs.
+        /// </summary>
+        /// <returns>a number greater than 0 for success, 0 for failure</returns>
+        static int GetSyncJobId(string syncJobName)
+        {
+            IEnumerable<KeyValuePair<int, TAS.SyncJobData>> syncJobs = TAS.SyncClient.GetAllSyncJobs();
+            foreach (KeyValuePair<int, TAS.SyncJobData> item in syncJobs)
+            {
+                if (item.Value.SyncJobName == syncJobName)
+                {
+                    return item.Key;
+                }
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Used to remove all the sync jobs and unset sync callbacks.
+        /// </summary>
+        public void RemoveAll()
+        {
+            IEnumerable<KeyValuePair<int, TAS.SyncJobData>> syncJobs = TAS.SyncClient.GetAllSyncJobs();
+            foreach (KeyValuePair<int, TAS.SyncJobData> item in syncJobs)
+            {
+                TAS.SyncClient.RemoveSyncJob(item.Key);
+            }
+
+            adapter = new TAS.SyncAdapter();
+            adapter.UnsetSyncEventCallbacks();
+        }
+
+        /// <summary>
+        /// Used to set sync callbacks.
+        /// </summary>
+        public void SetCallbacks()
+        {
+            Periodic = Calendar = Contact = Image = Music = Sound = Video = 0;
+
+            try
+            {
+                adapter = new TAS.SyncAdapter();
+                adapter.SetSyncEventCallbacks(StartSyncCallback, CancelSyncCallback);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[SampleSync] Err message : " + e.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Used to request On Demand sync job.
+        /// </summary>
+        public void OnDemand()
+        {
+            TAS.SyncJobData request = new TAS.SyncJobData();
+            request.SyncJobName = "RequestOnDemand";
+            // This id is used to remove pended On Demand sync job
+			// Generally, it is processed immediately if the device has network connection
+            int id = TAS.SyncClient.RequestOnDemandSyncJob(request, TAS.SyncOption.None);
+        }
+
+        /// <summary>
+        /// Used to add Periodic sync job.
+        /// </summary>
+        public void AddPeriodic()
+        {
+            TAS.SyncJobData request = new TAS.SyncJobData();
+            request.SyncJobName = "AddPeriodic";
+            // This sync job has an interval as 30 minutes
+            Periodic = TAS.SyncClient.AddPeriodicSyncJob(request, TAS.SyncPeriod.ThirtyMin, TAS.SyncOption.Expedited);
+        }
+
+        /// <summary>
+        /// Used to remove Periodic sync job.
+        /// </summary>
+        public void RemovePeriodic()
+        {
+            if (Periodic > 0)
+            {
+                TAS.SyncClient.RemoveSyncJob(Periodic);
+            }
+            else
+            {
+                TAS.SyncClient.RemoveSyncJob(GetSyncJobId("AddPeriodic"));
+            }
+            Periodic = 0;
+        }
+
+        /// <summary>
+        /// Used to add Calendar data change sync job.
+        /// </summary>
+        public void AddCalendarDataChange()
+        {
+            TAS.SyncJobData request = new TAS.SyncJobData();
+            request.SyncJobName = TAS.SyncJobData.CalendarCapability;
+			// If some data related with calendar is changed, you can know the time by using below API
+            Calendar = TAS.SyncClient.AddDataChangeSyncJob(request, TAS.SyncOption.Expedited);
+        }
+
+        /// <summary>
+        /// Used to add Contact data change sync job.
+        /// </summary>
+        public void AddContactDataChange()
+        {
+            TAS.SyncJobData request = new TAS.SyncJobData();
+            request.SyncJobName = TAS.SyncJobData.ContactCapability;
+			// If some data related with contact is changed, you can know the time by using below API
+            Contact = TAS.SyncClient.AddDataChangeSyncJob(request, TAS.SyncOption.Expedited);
+        }
+
+        /// <summary>
+        /// Used to add Image data change sync job.
+        /// </summary>
+        public void AddImageDataChange()
+        {
+            TAS.SyncJobData request = new TAS.SyncJobData();
+            request.SyncJobName = TAS.SyncJobData.ImageCapability;
+			// If some data related with image is changed, you can know the time by using below API
+            Image = TAS.SyncClient.AddDataChangeSyncJob(request, TAS.SyncOption.Expedited);
+        }
+
+        /// <summary>
+        /// Used to add Music data change sync job.
+        /// </summary>
+        public void AddMusicDataChange()
+        {
+            TAS.SyncJobData request = new TAS.SyncJobData();
+            request.SyncJobName = TAS.SyncJobData.MusicCapability;
+			// If some data related with music is changed, you can know the time by using below API
+            Music = TAS.SyncClient.AddDataChangeSyncJob(request, TAS.SyncOption.Expedited);
+        }
+
+        /// <summary>
+        /// Used to add Sound data change sync job.
+        /// </summary>
+        public void AddSoundDataChange()
+        {
+            TAS.SyncJobData request = new TAS.SyncJobData();
+            request.SyncJobName = TAS.SyncJobData.SoundCapability;
+			// If some data related with sound is changed, you can know the time by using below API
+            Sound = TAS.SyncClient.AddDataChangeSyncJob(request, TAS.SyncOption.Expedited);
+        }
+
+        /// <summary>
+        /// Used to add Video data change sync job.
+        /// </summary>
+        public void AddVideoDataChange()
+        {
+            TAS.SyncJobData request = new TAS.SyncJobData();
+            request.SyncJobName = TAS.SyncJobData.VideoCapability;
+			// If some data related with video is changed, you can know the time by using below API
+            Video = TAS.SyncClient.AddDataChangeSyncJob(request, TAS.SyncOption.Expedited);
+        }
+
+        /// <summary>
+        /// Used to remove Calendar data change sync job.
+        /// </summary>
+        public void RemoveCalendarDataChange()
+        {
+            if (Calendar > 0)
+            {
+                TAS.SyncClient.RemoveSyncJob(Calendar);
+            }
+            else
+            {
+                TAS.SyncClient.RemoveSyncJob(GetSyncJobId(TAS.SyncJobData.CalendarCapability));
+            }
+            Calendar = 0;
+        }
+
+        /// <summary>
+        /// Used to remove Contact data change sync job.
+        /// </summary>
+        public void RemoveContactDataChange()
+        {
+            if (Contact > 0)
+            {
+                TAS.SyncClient.RemoveSyncJob(Contact);
+            }
+            else
+            {
+                TAS.SyncClient.RemoveSyncJob(GetSyncJobId(TAS.SyncJobData.ContactCapability));
+            }
+            Contact = 0;
+        }
+
+        /// <summary>
+        /// Used to remove Image data change sync job.
+        /// </summary>
+        public void RemoveImageDataChange()
+        {
+            if (Image > 0)
+            {
+                TAS.SyncClient.RemoveSyncJob(Image);
+            }
+            else
+            {
+                TAS.SyncClient.RemoveSyncJob(GetSyncJobId(TAS.SyncJobData.ImageCapability));
+            }
+            Image = 0;
+        }
+
+        /// <summary>
+        /// Used to remove Music data change sync job.
+        /// </summary>
+        public void RemoveMusicDataChange()
+        {
+            if (Music > 0)
+            {
+                TAS.SyncClient.RemoveSyncJob(Music);
+            }
+            else
+            {
+                TAS.SyncClient.RemoveSyncJob(GetSyncJobId(TAS.SyncJobData.MusicCapability));
+            }
+            Music = 0;
+        }
+
+        /// <summary>
+        /// Used to remove Sound data change sync job.
+        /// </summary>
+        public void RemoveSoundDataChange()
+        {
+            if (Sound > 0)
+            {
+                TAS.SyncClient.RemoveSyncJob(Sound);
+            }
+            else
+            {
+                TAS.SyncClient.RemoveSyncJob(GetSyncJobId(TAS.SyncJobData.SoundCapability));
+            }
+            Sound = 0;
+        }
+
+        /// <summary>
+        /// Used to remove Video data change sync job.
+        /// </summary>
+        public void RemoveVideoDataChange()
+        {
+            if (Video > 0)
+            {
+                TAS.SyncClient.RemoveSyncJob(Video);
+            }
+            else
+            {
+                TAS.SyncClient.RemoveSyncJob(GetSyncJobId(TAS.SyncJobData.VideoCapability));
+            }
+            Video = 0;
+        }
+
+        /// <summary>
+        /// Used to register platform event
+        /// </summary>
+        /// <param name="_pEvent">platform event</param>
+        public static void EventRegister(IPlatformEvent _pEvent)
+        {
+            pEvent = _pEvent;
+        }
+    }
+}
+
---
+++ Mobile/SampleSync/SampleSync/SampleSync.Tizen.Mobile/SampleSync.Tizen.Mobile.cs
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using SampleSync.Tizen.Port;
+using SampleSync.Utils;
+
+namespace SampleSync.Tizen.Mobile
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        /// <summary>
+        /// An interface for the platform event.
+        /// </summary>
+        IPlatformEvent pEvent;
+
+        /// <summary>
+        /// This method is called when app is created.
+        /// </summary>
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+			var app = new App();
+
+            // To send event to UI
+			pEvent = app;
+            LoadApplication(app);
+            SampleSyncPort.EventRegister(pEvent);
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.DependencyService.Register<SampleSyncPort>();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
+
