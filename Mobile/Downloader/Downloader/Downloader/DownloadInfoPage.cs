debad39bde4e82d5cea1ce686f66b6452ebbfb8f
Seonah Moon <seonah1.moon@samsung.com>
2017-10-24 11:06:15 +0900

    [Downloader] move Downloader to Mobile directory

---
+++ Mobile/Downloader/Downloader/Downloader/DownloadInfoPage.cs
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace Downloader
+{
+    /// <summary>
+    ///  A public class to show download information page.
+    /// </summary>
+    public class DownloadInfoPage : ContentPage
+    {
+        private Label downloadInfoLabel;
+        private ListView downloadInfoListView;
+        private ObservableCollection<DownloadInfo> downloadInfoList = new ObservableCollection<DownloadInfo>();
+        private bool isUpdatedInfoList;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public DownloadInfoPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Refresh download information page.
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            UpdateDownloadInfoList();
+        }
+
+        /// <summary>
+        /// Initialize components of page.
+        /// </summary>
+        void InitializeComponent()
+        {
+            Title = "Download Info";
+            downloadInfoLabel = CreateDownloadInfoLabel();
+            downloadInfoListView = CreateListView();
+            isUpdatedInfoList = false;
+
+            Content = new StackLayout
+            {
+                Spacing = 0,
+                Children =
+                {
+                   downloadInfoLabel,
+                   downloadInfoListView,
+                }
+            };
+        }
+
+        /// <summary>
+        /// Update download information list.
+        /// This method is only called when download is completed.
+        /// </summary>
+        private void UpdateDownloadInfoList()
+        {
+            if (!isUpdatedInfoList && (State)DependencyService.Get<IDownload>().GetDownloadState() == State.Completed)
+            {
+                downloadInfoList.Add(new DownloadInfo("URL", DependencyService.Get<IDownload>().GetUrl()));
+                downloadInfoList.Add(new DownloadInfo("Content Name", DependencyService.Get<IDownload>().GetContentName()));
+                downloadInfoList.Add(new DownloadInfo("Content Size", DependencyService.Get<IDownload>().GetContentSize().ToString()));
+                downloadInfoList.Add(new DownloadInfo("MIME Type", DependencyService.Get<IDownload>().GetMimeType()));
+                downloadInfoList.Add(new DownloadInfo("Download Path", DependencyService.Get<IDownload>().GetDownloadedPath()));
+
+                isUpdatedInfoList = true;
+            }
+        }
+
+        /// <summary>
+        /// Create a new Label.
+        /// </summary>
+        /// <returns>Label</returns>
+        private Label CreateDownloadInfoLabel()
+        {
+            return new Label()
+            {
+                HorizontalTextAlignment = TextAlignment.Start,
+                Text = "Download Information",
+                TextColor = Color.White,
+                FontSize = 20,
+                Margin = new Thickness(10, 10),
+            };
+        }
+
+        /// <summary>
+        /// Create a new ListView
+        /// This "ListView" shows download information list.
+        /// </summary>
+        /// <returns>ListView</returns>
+        private ListView CreateListView()
+        {
+            ListView ListView = new ListView()
+            {
+                ItemsSource = downloadInfoList,
+
+                ItemTemplate = new DataTemplate(() =>
+                {
+                    Label nameLabel = new Label();
+                    nameLabel.SetBinding(Label.TextProperty, "Name");
+
+                    Label valueLabel = new Label();
+                    valueLabel.FontSize = 22;
+                    valueLabel.SetBinding(Label.TextProperty, "Value");
+
+                    return new ViewCell
+                    {
+                        View = new StackLayout
+                        {
+                            Padding = new Thickness(30, 20),
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                                  {
+                                      new StackLayout
+                                      {
+                                          VerticalOptions = LayoutOptions.Center,
+                                          Spacing = 0,
+                                          Children =
+                                          {
+                                              nameLabel,
+                                              valueLabel
+                                          }
+                                      }
+                                  }
+                        }
+                    };
+                }),
+            };
+            return ListView;
+        }
+    }
+}
---
+++ Mobile/Downloader/Downloader/Downloader/DownloadMainPage.cs
@@ -0,0 +1,235 @@
+﻿/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Xamarin.Forms;
+
+namespace Downloader
+{
+    /// <summary>
+    ///  A class for download main page.
+    /// </summary>
+    class DownloadMainPage : ContentPage
+    {
+        private View urlEntryView;
+        private EntryCell urlEntryCell;
+        private Button downloadButton;
+        private ProgressBar progressBar;
+        private Label progressLabel;
+        private string downloadUrl;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DownloadMainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Initialize main page. 
+        /// Add components and events.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Title = "Download";
+            IsVisible = true;
+
+            BackgroundColor = Color.White;
+
+            urlEntryCell = CreateEntryCell();
+            urlEntryView = CreateEntryView();
+            downloadButton = CreateDownloadButton();
+            progressBar = CreateProgressbar();
+            progressLabel = CreateProgressLabel();
+
+            AddEvent();
+
+            Content = new StackLayout
+            {
+                Spacing = 0,
+                Children =
+                {
+                   urlEntryView,
+                   downloadButton,
+                   progressBar,
+                   progressLabel,
+                }
+            };
+        }
+
+        /// <summary>
+        /// Create a new EntryCell to get URL.
+        /// </summary>
+        /// <returns>EntryCell</returns>
+        private EntryCell CreateEntryCell()
+        {
+            return new EntryCell()
+            {
+                Label = "URL: ",
+                Placeholder = "Type URL here to download",
+                Height = 400,
+                Keyboard = Keyboard.Url,
+            };
+        }
+
+        /// <summary>
+        /// Create a new EntryView.
+        /// </summary>
+        /// <returns>EntryView</returns>
+        private View CreateEntryView()
+        {
+            return new TableView()
+            {
+                HeightRequest = 200,
+                Intent = TableIntent.Form,
+                Margin = new Thickness(10, 10),
+                VerticalOptions = LayoutOptions.Start,
+                Root = new TableRoot
+                {
+                    new TableSection
+                    {
+                        urlEntryCell
+                    },
+                }
+            };
+        }
+
+        /// <summary>
+        /// Create a new Button to start download.
+        /// </summary>
+        /// <returns>Button</returns>
+        private Button CreateDownloadButton()
+        {
+            return new Button()
+            {
+                Text = "Start Download",
+                Font = Font.SystemFontOfSize(NamedSize.Default),
+                FontAttributes = FontAttributes.Bold,
+                BorderWidth = 1,
+                VerticalOptions = LayoutOptions.Start,
+            };
+        }
+
+        /// <summary>
+        /// Create a new ProgressBar to present download state.
+        /// </summary>
+        /// <returns><ProgressBar</returns>
+        private ProgressBar CreateProgressbar()
+        {
+            return new ProgressBar()
+            {
+                HeightRequest = 100,
+                Progress = .0,
+                VerticalOptions = LayoutOptions.Start,
+            };
+        }
+
+        /// <summary>
+        /// Create a new Label to present receiving data size in bytes.
+        /// </summary>
+        /// <returns>Label</returns>
+        private Label CreateProgressLabel()
+        {
+            return new Label()
+            {
+                BackgroundColor = Color.Transparent,
+                HorizontalTextAlignment = TextAlignment.End,
+                Text = "0 bytes / 0 bytes",
+                FontAttributes = FontAttributes.Bold,
+                VerticalOptions = LayoutOptions.Start,
+            };
+        }
+
+        /// <summary>
+        /// Register event handlers for entry, button and state callback.
+        /// </summary>
+        private void AddEvent()
+        {
+            urlEntryCell.Completed += OnEntryCellCompleted;
+            downloadButton.Clicked += OnButtonClicked;
+            DependencyService.Get<IDownload>().DownloadStateChanged += OnStateChanged;
+            DependencyService.Get<IDownload>().DownloadProgress += OnProgressbarChanged;
+        }
+
+        /// <summary>
+        /// Event handler when EntryCell is completed.
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event Arguments</param>
+        private void OnEntryCellCompleted(object sender, EventArgs e)
+        {
+            DependencyService.Get<IDownload>().DownloadLog("OnEntryCellCompleted()");
+            downloadButton.Text = "Download Start";
+        }
+
+        /// <summary>
+        /// Event handler when download state is changed.
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments including download state</param>
+        private void OnStateChanged(object sender, DownloadStateChangedEventArgs e)
+        {
+            if (e.stateMsg.Length > 0)
+            {
+                DependencyService.Get<IDownload>().DownloadLog("State: " + e.stateMsg);
+                if (e.stateMsg != downloadButton.Text)
+                {
+                    downloadButton.Text = e.stateMsg;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event handler when data is received.
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments including received data size</param>
+        private void OnProgressbarChanged(object sender, DownloadProgressEventArgs e)
+        {
+            ulong ContentSize =  DependencyService.Get<IDownload>().GetContentSize();
+
+            if (e.ReceivedSize > 0)
+            {
+                progressBar.Progress = (double)(e.ReceivedSize / ContentSize);
+                progressLabel.Text = e.ReceivedSize + "bytes / " + ContentSize + "bytes";
+            }
+        }
+
+        /// <summary>
+        /// Event handler when button is clicked.
+        /// Once a button is clicked and download is started, a button is disabled.
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            downloadUrl = urlEntryCell.Text;
+
+            DependencyService.Get<IDownload>().DownloadLog("Start Download: " + downloadUrl);
+            try
+            {
+                DependencyService.Get<IDownload>().StartDownload(downloadUrl);
+                ((Button)sender).IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                DependencyService.Get<IDownload>().DownloadLog("Request.Start() is failed" + ex);
+                ((Button)sender).IsEnabled = true;
+            }
+        }
+    }
+}
