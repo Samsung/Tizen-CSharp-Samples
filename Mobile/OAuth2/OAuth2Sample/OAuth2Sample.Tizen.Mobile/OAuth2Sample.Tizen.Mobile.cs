0931272f69b04433e1c14ab59ef8502de95e82b5
jkjo <31117237+jkjo@users.noreply.github.com>
2017-11-22 16:08:02 +0900

    Create OAuth2Sample.Tizen.Mobile.cs

---
+++ Mobile/OAuth2/OAuth2Sample/OAuth2Sample.Tizen.Mobile/OAuth2Sample.Tizen.Mobile.cs
@@ -0,0 +1,21 @@
+using System;
+using OAuth2Sample.Tizen.Mobile.Port;
+
+namespace OAuth2Sample.Tizen.Mobile
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
