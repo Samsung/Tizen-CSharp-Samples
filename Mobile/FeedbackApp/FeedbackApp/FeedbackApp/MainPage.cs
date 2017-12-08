5493b1599ada99d8001f0d37fc8165dbb9289b07
pr.jung <pr.jung@samsung.com>
2017-12-08 17:05:09 +0900

    [FeedbackApp] Add Feedback Sample Application
    Signed-off-by: pr.jung <pr.jung@samsung.com>

---
+++ Mobile/FeedbackApp/FeedbackApp/FeedbackApp/MainPage.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace FeedbackApp
+{
+    /// <summary>
+    /// Main page of Feedback application
+    /// User can select feedback patterns
+    /// </summary>
+    class MainPage : ContentPage
+    {
+        /// <summary>
+        /// Asynchronous method for tapped event of pattern item
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="a">Event argument</param>
+        private async void Pattern_ItemTappedAsync(object sender, ItemTappedEventArgs a)
+        {
+            // Create SecondPage with user selected pattern
+            await this.Navigation.PushAsync(new SecondPage(a.Item.ToString()));
+        }
+
+        /// <summary>
+        /// The constructor of MainPage class
+        /// Feedback patterns are listed
+        ///     Tap
+        ///     SoftInputPanel
+        ///     Key0
+        ///     Key1
+        ///     Key2
+        ///     Key3
+        ///     Key4
+        ///     Key5
+        ///     Key6
+        ///     Key7
+        ///     Key8
+        ///     Key9
+        ///     KeyStar
+        ///     KeySharp
+        ///     KeyBack
+        ///     Hold
+        ///     HardwareKeyPressed
+        ///     HardwareKeyHold
+        ///     Message
+        ///     Email
+        ///     WakeUp
+        ///     Schedule
+        ///     Timer
+        ///     General
+        ///     PowerOn
+        ///     PowerOff
+        ///     ChargerConnected
+        ///     ChargingError
+        ///     FullyCharged
+        ///     LowBattery
+        ///     Lock
+        ///     UnLock
+        ///     VibrationModeAbled
+        ///     SilentModeDisabled
+        ///     BluetoothDeviceConnected
+        ///     BluetoothDeviceDisconnected
+        ///     ListReorder
+        ///     ListSlider
+        ///     VolumeKeyPressed
+        /// </summary>
+        public MainPage()
+        {
+            // Title of this page is Patterns
+            this.Title = "Patterns";
+            var MainListView = new ListView();
+
+            // Create a new ListView of pattern strings
+            MainListView.ItemsSource = new string[]
+            {
+                "Tap",
+                "SoftInputPanel",
+                "Key0",
+                "Key1",
+                "Key2",
+                "Key3",
+                "Key4",
+                "Key5",
+                "Key6",
+                "Key7",
+                "Key8",
+                "Key9",
+                "KeyStar",
+                "KeySharp",
+                "KeyBack",
+                "Hold",
+                "HardwareKeyPressed",
+                "HardwareKeyHold",
+                "Message",
+                "Email",
+                "WakeUp",
+                "Schedule",
+                "Timer",
+                "General",
+                "PowerOn",
+                "PowerOff",
+                "ChargerConnected",
+                "ChargingError",
+                "FullyCharged",
+                "LowBattery",
+                "Lock",
+                "UnLock",
+                "VibrationModeAbled",
+                "SilentModeDisabled",
+                "BluetoothDeviceConnected",
+                "BluetoothDeviceDisconnected",
+                "ListReorder",
+                "ListSlider",
+                "VolumeKeyPressed",
+            };
+
+            // Add ItemTapped event
+            MainListView.ItemTapped += Pattern_ItemTappedAsync;
+            this.Content = MainListView;
+        }
+    }
+}
---
+++ Mobile/FeedbackApp/FeedbackApp/FeedbackApp/ResultPage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FeedbackApp
+{
+    /// <summary>
+    /// Result page of Feedback application
+    /// </summary>
+    class ResultPage : ContentPage
+    {
+        /// <summary>
+        /// Create a Label instance
+        /// </summary>
+        /// <param name="text">Text string for label</param>
+        /// <returns>Created Label</returns>
+        private Label CreateLabel(string text)
+        {
+            // Create a new Label for given text
+            return new Label
+            {
+                Text = text,
+                HorizontalTextAlignment = TextAlignment.Center,
+                FontSize = 25
+            };
+        }
+
+        /// <summary>
+        /// The constructor of ResultPage
+        /// </summary>
+        /// <param name="pattern">User tapped pattern</param>
+        /// <param name="result">Feedback play result</param>
+        public ResultPage(string pattern, bool result)
+        {
+            Label label, label2;
+
+            // Label for pattern string
+            label = CreateLabel(pattern);
+
+            // Create different label as feedback play result
+            // Success case
+            if (result)
+            {
+                // Label for Feedback play success case
+                label2 = CreateLabel("Play Succeed");
+            }
+            // Fail case
+            else
+            {
+                // Label for Feedback play fail case
+                label2 = CreateLabel("Not Supported");
+            }
+
+            // Create a StackLayout with Labels
+            // Show label and label2 on different line
+            this.Content = new StackLayout()
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                Children =
+                {
+                    label,
+                    label2,
+                }
+            };
+        }
+    }
+}
