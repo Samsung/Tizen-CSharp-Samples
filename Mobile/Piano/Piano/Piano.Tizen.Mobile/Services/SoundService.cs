b90ff3ffb78062a26553e4136ccd5f7edd5b01d9
Michal Kolodziejski <m.kolodziejs@samsung.com>
2018-01-10 14:08:29 +0100

    [Piano][Mobile] New sample app
    Signed-off-by: Michal Kolodziejski <m.kolodziejs@samsung.com>

---
+++ Mobile/Piano/Piano/Piano.Tizen.Mobile/Services/SoundService.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Piano.Models;
+using System.Threading.Tasks;
+using Piano.Tizen.Mobile.Services;
+using Tizen.Applications;
+using Tizen.Multimedia;
+
+[assembly: Xamarin.Forms.Dependency(typeof(SoundService))]
+
+namespace Piano.Tizen.Mobile.Services
+{
+    /// <summary>
+    /// Service that plays sounds.
+    /// </summary>
+    public class SoundService : ISound
+    {
+        #region fields
+
+        /// <summary>
+        /// Maximum sound index.
+        /// </summary>
+        private const int MAX_SOUND_INDEX = 13;
+
+        /// <summary>
+        /// Indicates if player is initialized.
+        /// </summary>
+        private bool _initialized;
+
+        /// <summary>
+        /// Contains every sounds.
+        /// </summary>
+        private Player[] _sounds;
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Initializes sound player.
+        /// </summary>
+        /// <returns>Task with initialization.</returns>
+        public async Task Init()
+        {
+            if (_initialized)
+            {
+                return;
+            }
+
+            var resPath = Application.Current.DirectoryInfo.Resource;
+            _sounds = new Player[MAX_SOUND_INDEX + 1];
+
+            for (var i = 0; i <= MAX_SOUND_INDEX; i++)
+            {
+                var player = new Player
+                {
+                    IsLooping = false
+                };
+
+                player.SetSource(new MediaUriSource($"file://{resPath}/sounds/{i}.wav"));
+
+                await player.PrepareAsync();
+
+                _sounds[i] = player;
+            }
+
+            _initialized = true;
+        }
+
+        /// <summary>
+        /// Plays sound with given index.
+        /// </summary>
+        /// <param name="soundIndex">Sound index.</param>
+        /// <returns>
+        /// Task with bool result.
+        /// Result is true if playing was successful, false otherwise.
+        /// </returns>
+        public async Task<bool> Play(int soundIndex)
+        {
+            if (!_initialized || soundIndex < 0 || soundIndex > MAX_SOUND_INDEX)
+            {
+                return false;
+            }
+
+            var player = _sounds[soundIndex];
+
+            if (player.State == PlayerState.Idle)
+            {
+                return false;
+            }
+
+            await player.SetPlayPositionAsync(0, false);
+
+            player.Start();
+
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ Mobile/Piano/Piano/Piano/Controls/Key.xaml.cs
@@ -0,0 +1,149 @@
+﻿/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Xamarin.Forms;
+
+namespace Piano.Controls
+{
+    /// <summary>
+    /// Interaction logic for Key.xaml.
+    /// </summary>
+    public partial class Key
+    {
+        #region fields
+
+        /// <summary>
+        /// Local storage of <see cref="SoundNumber"/>
+        /// </summary>
+        private int _soundNumber;
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Bindable property that allows to set image source for pressed state.
+        /// </summary>
+        public static BindableProperty PressedImageSourceProperty =
+            BindableProperty.Create(nameof(PressedImageSource), typeof(string), typeof(Key));
+
+        /// <summary>
+        /// Bindable property that allows to set image source for unpressed state.
+        /// </summary>
+        public static BindableProperty UnPressedImageSourceProperty =
+            BindableProperty.Create(nameof(UnPressedImageSource), typeof(string), typeof(Key),
+                propertyChanged: UnPressedImageSourcePropertyChanged);
+
+        /// <summary>
+        /// Gets or sets sound index that will be played.
+        /// </summary>
+        public int SoundNumber
+        {
+            get => _soundNumber;
+            set
+            {
+                _soundNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets image source for pressed state.
+        /// </summary>
+        public string PressedImageSource
+        {
+            get => GetValue(PressedImageSourceProperty).ToString();
+            set => SetValue(PressedImageSourceProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets image source for unpressed state.
+        /// </summary>
+        public string UnPressedImageSource
+        {
+            get => GetValue(UnPressedImageSourceProperty).ToString();
+            set => SetValue(UnPressedImageSourceProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets command that is executed on click.
+        /// </summary>
+        public Command OnClickedCommand { get; set; }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Default class constructor.
+        /// </summary>
+        public Key()
+        {
+            InitializeComponent();
+
+            var tapGestureRecognizer = new TapGestureRecognizer
+            {
+                Command = new Command(ExecuteClickCommand)
+            };
+
+            GestureRecognizers.Add(tapGestureRecognizer);
+        }
+
+        /// <summary>
+        /// Invoked on image source change.
+        /// Updates image source.
+        /// </summary>
+        /// <param name="bindable">The bindable object that contains the property.</param>
+        /// <param name="oldValue">Old value of the property.</param>
+        /// <param name="newValue">New value of the property.</param>
+        private static void UnPressedImageSourcePropertyChanged(BindableObject bindable, object oldValue,
+            object newValue)
+        {
+            if (bindable is Key key)
+            {
+                key.UpdateImageSource(newValue.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Executed on image click.
+        /// </summary>
+        private void ExecuteClickCommand()
+        {
+            UpdateImageSource(PressedImageSource);
+
+            OnClickedCommand?.Execute(null);
+
+            Device.StartTimer(new TimeSpan(0, 0, 0, 0, 300), () =>
+            {
+                UpdateImageSource(UnPressedImageSource);
+                return false;
+            });
+        }
+
+        /// <summary>
+        /// Updates image source.
+        /// </summary>
+        /// <param name="newSource">New image source.</param>
+        private void UpdateImageSource(string newSource)
+        {
+            Source = ImageSource.FromFile(newSource);
+        }
+
+        #endregion
+    }
+}
