2653730700d3862aab25b8707ae540523cd56532
Kim HyunWoo <stories2@naver.com>
2018-01-14 03:04:43 +0900

    App module : simple finding good place solution

--- Mobile/NoughtsAndCrosses/NoughtsAndCrosses/NoughtsAndCrosses/NoughtsAndCrosses.cs
+++ Mobile/NoughtsAndCrosses/NoughtsAndCrosses/NoughtsAndCrosses/NoughtsAndCrosses.cs
@@ -46,6 +46,7 @@ namespace NoughtsAndCrosses
         public void ChangeGameBoardBtnText(Button button, int row, int col)
         {
             gameboard[row, col] = MainLayout.HUMAN_SELECTED;
+            FindGoodPlace();
             mainLayout.SetGameBoard(gameboard);
         }
 
@@ -60,6 +61,96 @@ namespace NoughtsAndCrosses
                 }
             }
             mainLayout.SetGameBoard(gameboard);
+        }
+
+        void FindGoodPlace()
+        {
+            int i, t, maxScore = -0x7fffffff, maxScoreRow = MainLayout.ZERO, maxScoreCol = MainLayout.ZERO;
+            int[,] tempGameboard = new int[,]{
+                {MainLayout.EMPTY, MainLayout.EMPTY, MainLayout.EMPTY },
+                {MainLayout.EMPTY, MainLayout.EMPTY, MainLayout.EMPTY },
+                {MainLayout.EMPTY, MainLayout.EMPTY, MainLayout.EMPTY }
+            };
+            for (i = MainLayout.ZERO; i < MainLayout.GAMEBOARD_SIZE; i += 1)
+            {
+                for (t = MainLayout.ZERO; t < MainLayout.GAMEBOARD_SIZE; t += 1)
+                {
+                    if (gameboard[i, t] == MainLayout.HUMAN_SELECTED)
+                    {
+                        if (i > 0)//N
+                        {
+                            if (gameboard[i - 1, t] == MainLayout.EMPTY)
+                            {
+                                tempGameboard[i - 1, t] += 1;
+                            }
+                        }
+                        if (i > 0 && t < MainLayout.GAMEBOARD_SIZE - 1)//NE
+                        {
+                            if (gameboard[i - 1, t + 1] == MainLayout.EMPTY)
+                            {
+                                tempGameboard[i - 1, t + 1] += 1;
+                            }
+                        }
+                        if (t < MainLayout.GAMEBOARD_SIZE - 1)//E
+                        {
+                            if (gameboard[i, t + 1] == MainLayout.EMPTY)
+                            {
+                                tempGameboard[i, t + 1] += 1;
+                            }
+                        }
+                        if(i < MainLayout.GAMEBOARD_SIZE - 1 && t < MainLayout.GAMEBOARD_SIZE - 1)//SE
+                        {
+                            if(gameboard[i + 1, t + 1] == MainLayout.EMPTY)
+                            {
+                                tempGameboard[i + 1, t + 1] += 1;
+                            }
+                        }
+                        if (i < MainLayout.GAMEBOARD_SIZE - 1)//S
+                        {
+                            if (gameboard[i + 1, t] == MainLayout.EMPTY)
+                            {
+                                tempGameboard[i + 1, t] += 1;
+                            }
+                        }
+                        if(i < MainLayout.GAMEBOARD_SIZE - 1 && t > 0)//SW
+                        {
+                            if(gameboard[i + 1, t - 1] == MainLayout.EMPTY)
+                            {
+                                tempGameboard[i + 1, t - 1] += 1;
+                            }
+                        }
+                        if (t > 0)
+                        {
+                            if (gameboard[i, t - 1] == MainLayout.EMPTY)//W
+                            {
+                                tempGameboard[i, t - 1] += 1;
+                            }
+                        }
+                        if(i > 0 && t > 0)
+                        {
+                            if(gameboard[i - 1, t - 1] == MainLayout.EMPTY)//NW
+                            {
+                                tempGameboard[i - 1, t - 1] += 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (i = MainLayout.ZERO; i < MainLayout.GAMEBOARD_SIZE; i += 1)
+            {
+                for (t = MainLayout.ZERO; t < MainLayout.GAMEBOARD_SIZE; t += 1)
+                {
+                    if(maxScore < tempGameboard[i, t])
+                    {
+                        maxScore = tempGameboard[i, t];
+                        maxScoreRow = i;
+                        maxScoreCol = t;
+                    }
+                }
+            }
+
+            gameboard[maxScoreRow, maxScoreCol] = MainLayout.COMPUTER_SELECTED;
         }
     }
 }
