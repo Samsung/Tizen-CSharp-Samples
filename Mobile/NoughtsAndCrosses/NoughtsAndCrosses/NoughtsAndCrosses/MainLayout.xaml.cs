9ea884d51772e4aa368c4c07e541a0b9bd660be8
Kim HyunWoo <stories2@naver.com>
2018-01-14 02:21:18 +0900

    App module : control user input events

--- Mobile/NoughtsAndCrosses/NoughtsAndCrosses/NoughtsAndCrosses/MainLayout.xaml.cs
+++ Mobile/NoughtsAndCrosses/NoughtsAndCrosses/NoughtsAndCrosses/MainLayout.xaml.cs
@@ -12,6 +12,14 @@ namespace NoughtsAndCrosses
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainLayout : ContentPage
 	{
+        public static readonly int
+            EMPTY = 0,
+            COMPUTER_SELECTED = 1,
+            HUMAN_SELECTED = 2,
+            
+            ZERO = 0,
+            
+            GAMEBOARD_SIZE = 3;
         public App app;
 
 		public MainLayout ()
@@ -32,7 +40,27 @@ namespace NoughtsAndCrosses
 
         public void SetGameBoard(int[, ] gameboardArray)
         {
-            
+            int i, t, q = ZERO;
+            for(i = ZERO; i < GAMEBOARD_SIZE; i += 1)
+            {
+                for(t = ZERO; t < GAMEBOARD_SIZE; t += 1)
+                {
+                    Button indexOfButton = (Button)gameboard.Children.ElementAt(q);
+                    if(gameboardArray[i, t] == HUMAN_SELECTED)
+                    {
+                        indexOfButton.Text = "O";
+                    }
+                    else if(gameboardArray[i, t] == COMPUTER_SELECTED)
+                    {
+                        indexOfButton.Text = "X";
+                    }
+                    else
+                    {
+                        indexOfButton.Text = "";
+                    }
+                    q += 1;
+                }
+            }
         }
 
         public void HandleGameBoardBtnClick(Object sender, System.EventArgs e)
--- Mobile/NoughtsAndCrosses/NoughtsAndCrosses/NoughtsAndCrosses/NoughtsAndCrosses.cs
+++ Mobile/NoughtsAndCrosses/NoughtsAndCrosses/NoughtsAndCrosses/NoughtsAndCrosses.cs
@@ -10,12 +10,21 @@ namespace NoughtsAndCrosses
     public class App : Application
     {
         public MainLayout mainLayout;
+
+        int[, ] gameboard = new int[,]{
+            {MainLayout.EMPTY, MainLayout.EMPTY, MainLayout.EMPTY },
+            {MainLayout.EMPTY, MainLayout.EMPTY, MainLayout.EMPTY },
+            {MainLayout.EMPTY, MainLayout.EMPTY, MainLayout.EMPTY }
+        };
+
         public App()
         {
             // The root page of your application
             mainLayout = new MainLayout(this) { };
 
             MainPage = mainLayout;
+
+            mainLayout.SetGameBoard(gameboard);
         }
 
         protected override void OnStart()
@@ -36,12 +45,21 @@ namespace NoughtsAndCrosses
 
         public void ChangeGameBoardBtnText(Button button, int row, int col)
         {
-            button.Text = "(" + row + ", " + col + ")";
+            gameboard[row, col] = MainLayout.HUMAN_SELECTED;
+            mainLayout.SetGameBoard(gameboard);
         }
 
         public void ChangeResetBtnText(Button button)
         {
-            button.Text = "Resetting...";
+            int i, t;
+            for(i = MainLayout.ZERO; i < MainLayout.GAMEBOARD_SIZE; i += 1)
+            {
+                for(t = MainLayout.ZERO; t < MainLayout.GAMEBOARD_SIZE; t += 1)
+                {
+                    gameboard[i, t] = MainLayout.EMPTY;
+                }
+            }
+            mainLayout.SetGameBoard(gameboard);
         }
     }
 }
