c2afdcdb41928b7a6ddc5bf5e2a71188c8544421
Lynettejin90 <33114201+Lynettejin90@users.noreply.github.com>
2018-03-20 15:17:31 +0800

    [ImageSample][TV]ImageSample - new sample application

---
+++ TV/ImageSample/ImageSample/src/Samples/NinePatchSample.cs
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System;
+using Tizen;
+using System.Runtime.InteropServices;
+using Tizen.NUI;
+using Tizen.NUI.UIComponents;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Constants;
+
+namespace ImageSample
+{
+    /// <summary>
+    /// A sample of 9patch, to show ninepatch file is supported in dali.
+    /// </summary>
+    class NinePatchSample : IExample
+    {
+        private const string resources = "/home/owner/apps_rw/org.tizen.example.ImageSample/res/images/";
+        private string imagePath = resources + "heartsframe.9.png";
+        private TextLabel guide, userGuide;
+
+        private ImageView small, middle, large;
+
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        public NinePatchSample()
+        {
+        }
+
+        /// <summary>
+        /// Text Sample Application initialisation.
+        /// </summary>
+        private void Initialize()
+        {
+            Window.Instance.BackgroundColor = Color.Black;
+            //Title to show the sample name.
+            guide = new TextLabel();
+            guide.HorizontalAlignment = HorizontalAlignment.Center;
+            guide.VerticalAlignment = VerticalAlignment.Center;
+            guide.PositionUsesPivotPoint = true;
+            guide.ParentOrigin = ParentOrigin.TopLeft;
+            guide.PivotPoint = PivotPoint.TopLeft;
+            guide.Size2D = new Size2D(1920, 100);
+            guide.FontFamily = "Samsung One 600";
+            guide.Position2D = new Position2D(0, 0);
+            guide.MultiLine = false;
+            guide.PointSize = 15.0f;
+            guide.Text = "NinePatch Sample \n";
+            guide.TextColor = Color.White;
+            guide.BackgroundColor = new Color(43.0f / 255.0f, 145.0f / 255.0f, 175.0f / 255.0f, 1.0f);
+            Window.Instance.GetDefaultLayer().Add(guide);
+
+            userGuide = new TextLabel();
+            userGuide.HorizontalAlignment = HorizontalAlignment.Begin;
+            userGuide.VerticalAlignment = VerticalAlignment.Top;
+            userGuide.PositionUsesPivotPoint = true;
+            userGuide.ParentOrigin = ParentOrigin.TopLeft;
+            userGuide.PivotPoint = PivotPoint.TopLeft;
+            userGuide.FontFamily = "Samsung One 400";
+            userGuide.Position = new Position(30, 150, 0);
+            userGuide.MultiLine = true;
+            userGuide.PointSize = 10.0f;
+            userGuide.Text = "The following three ImageView are created with the same image file but with different size.\n";
+            userGuide.TextColor = Color.White;
+            Window.Instance.GetDefaultLayer().Add(userGuide);
+
+            //Create three imageView with different size, samll, middle, and large, to show the same 9patch file.
+            //This imageView's size is (124, 85). it's the small one.
+            small = new ImageView(imagePath);
+            small.ParentOrigin = ParentOrigin.TopLeft;
+            small.PivotPoint = PivotPoint.TopLeft;
+            small.Size2D = new Size2D(124, 85);
+            small.Position2D = new Position2D(898, 272);
+            Window.Instance.GetDefaultLayer().Add(small);
+            //This imageView's size is (249, 169). it's the middle one.
+            middle = new ImageView(imagePath);
+            middle.ParentOrigin = ParentOrigin.TopLeft;
+            middle.PivotPoint = PivotPoint.TopLeft;
+            middle.Size2D = new Size2D(249, 169);
+            middle.Position2D = new Position2D(835, 429);
+            Window.Instance.GetDefaultLayer().Add(middle);
+            //This imageView's size is (498, 338). it's the large one.
+            large = new ImageView(imagePath);
+            large.ParentOrigin = ParentOrigin.TopLeft;
+            large.PivotPoint = PivotPoint.TopLeft;
+            large.Size2D = new Size2D(498, 338);
+            large.Position2D = new Position2D(711, 670);
+            Window.Instance.GetDefaultLayer().Add(large);
+
+        }
+
+        /// <summary>
+        /// Remove all the view from window, and Dispose three imageView and guide.
+        /// </summary>
+        public void Deactivate()
+        {
+
+            Window.Instance.GetDefaultLayer().Remove(guide);
+            guide.Dispose();
+            guide = null;
+
+            Window.Instance.GetDefaultLayer().Remove(userGuide);
+            userGuide.Dispose();
+            userGuide = null;
+
+            Window.Instance.GetDefaultLayer().Remove(small);
+            small.Dispose();
+            small = null;
+
+            Window.Instance.GetDefaultLayer().Remove(middle);
+            middle.Dispose();
+            middle = null;
+
+            Window.Instance.GetDefaultLayer().Remove(large);
+            large.Dispose();
+            large = null;
+        }
+
+        /// <summary>
+        /// Activate this class.
+        /// </summary>
+        public void Activate()
+        {
+            Tizen.Log.Fatal("NUI", "Activate");
+            Initialize();
+        }
+    }
+}
---
+++ TV/ImageSample/ImageSample/src/Samples/SvgSample.cs
@@ -0,0 +1,270 @@
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System;
+using Tizen;
+using System.Runtime.InteropServices;
+using Tizen.NUI;
+using Tizen.NUI.UIComponents;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Constants;
+
+namespace ImageSample
+{
+    /// <summary>
+    /// A sample to show the svg file.
+    /// </summary>
+    class SvgSample : IExample
+    {
+        private const string resources = "/home/owner/apps_rw/org.tizen.example.ImageSample/res/images/";
+        private string normalImagePath = resources + "Button/btn_bg_25_25_25_95.9.png";
+        private string focusImagePath = resources + "Button/btn_bg_255_255_255_200.9.png";
+        private string pressImagePath = resources + "Button/btn_bg_0_129_198_100.9.png";
+        private String image_svg = resources + "Kid1.svg";
+        private TextLabel guide;
+        private float svgScale = 1;
+        private ImageView svgImage;
+        private PushButton zoomInButton, zoomOutButton;
+        // <summary>
+        /// Constructor to create new RadioButtonSample
+        /// </summary>
+        public SvgSample()
+        {
+        }
+
+        /// <summary>
+        /// RadioButton initialisation.
+        /// </summary>
+        private void Initialize()
+        {
+            Window.Instance.BackgroundColor = Color.Black;
+            //Create Title to show the sample name.
+            guide = new TextLabel();
+            guide.HorizontalAlignment = HorizontalAlignment.Center;
+            guide.VerticalAlignment = VerticalAlignment.Center;
+            guide.PositionUsesPivotPoint = true;
+            guide.ParentOrigin = ParentOrigin.TopLeft;
+            guide.PivotPoint = PivotPoint.TopLeft;
+            guide.Size2D = new Size2D(1920, 100);
+            guide.FontFamily = "Samsung One 600";
+            guide.Position2D = new Position2D(0, 0);
+            guide.MultiLine = false;
+            guide.PointSize = 15.0f;
+            guide.Text = "Svg Sample \n";
+            guide.TextColor = Color.White;
+            guide.BackgroundColor = new Color(43.0f / 255.0f, 145.0f / 255.0f, 175.0f / 255.0f, 1.0f);
+            Window.Instance.GetDefaultLayer().Add(guide);
+
+            //Create an imageView to show the svg file.
+            svgImage = new ImageView(image_svg);
+            svgImage.SizeWidth = 200;
+            svgImage.SizeHeight = 200;
+            svgImage.PositionUsesPivotPoint = true ;
+            svgImage.PivotPoint = PivotPoint.TopLeft;
+            svgImage.ParentOrigin = ParentOrigin.TopLeft;
+            svgImage.Position2D = new Position2D(860, 400);
+            Window.Instance.GetDefaultLayer().Add(svgImage);
+
+            // Create zoomInButton which can make svgImage bigger
+            zoomInButton = CreateButton("Zoom In", "Zoom In");
+            zoomInButton.PositionUsesPivotPoint = true;
+            zoomInButton.PivotPoint = PivotPoint.TopLeft;
+            zoomInButton.ParentOrigin = ParentOrigin.TopLeft;
+            zoomInButton.Position2D = new Position2D(460, 900);
+            zoomInButton.Clicked += ZoomInButtonClick;
+            zoomInButton.Focusable = true;
+            Window.Instance.GetDefaultLayer().Add(zoomInButton);
+
+            // Create zoomOutButton which can make svgImage smaller
+            zoomOutButton = CreateButton("Zoom Out", "Zoom Out");
+            zoomOutButton.PositionUsesPivotPoint = true;
+            zoomOutButton.PivotPoint = PivotPoint.TopLeft;
+            zoomOutButton.ParentOrigin = ParentOrigin.TopLeft;
+            zoomOutButton.Position2D = new Position2D(1060, 900);
+            zoomOutButton.Clicked += ZoomOutButtonClick;
+            zoomOutButton.Focusable = true;
+            Window.Instance.GetDefaultLayer().Add(zoomOutButton);
+
+            zoomInButton.RightFocusableView = zoomOutButton;
+            zoomOutButton.LeftFocusableView = zoomInButton;
+
+            FocusManager.Instance.SetCurrentFocusView(zoomInButton);
+        }
+
+        /// <summary>
+        /// Dispose radioButtonGroupView and guide
+        /// </summary>
+        public void Deactivate()
+        {
+
+            Window.Instance.GetDefaultLayer().Remove(guide);
+            guide.Dispose();
+            guide = null;
+
+            Window.Instance.GetDefaultLayer().Remove(svgImage);
+            svgImage.Dispose();
+            svgImage = null;
+
+            Window.Instance.GetDefaultLayer().Remove(zoomInButton);
+            zoomInButton.Dispose();
+            zoomInButton = null;
+
+            Window.Instance.GetDefaultLayer().Remove(zoomOutButton);
+            zoomOutButton.Dispose();
+            zoomOutButton = null;
+
+        }
+
+        /// <summary>
+        /// Activate this class.
+        /// </summary>
+        public void Activate()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// The event will be triggered when  have key clicked and focus on spin.
+        /// </summary>
+        /// <param name="name">the name of the button.</param>
+        /// <param name="text">the text of the button</param>
+        /// <returns>The consume flag</returns>
+        private PushButton CreateButton(string name, string text)
+        {
+            PushButton button = new PushButton();
+            button.Focusable = true;
+            button.Size2D = new Size2D(400, 80);
+            button.Focusable = true;
+            button.Name = name;
+            // Create the label which will show when _pushbutton focused.
+            PropertyMap _focusText = CreateTextVisual(text, Color.Black);
+
+            // Create the label which will show when _pushbutton unfocused.
+            PropertyMap _unfocusText = CreateTextVisual(text, Color.White);
+            button.Label = _unfocusText;
+
+            // Create normal background visual.
+            PropertyMap normalMap = CreateImageVisual(normalImagePath);
+
+            // Create focused background visual.
+            PropertyMap focusMap = CreateImageVisual(focusImagePath);
+
+            // Create pressed background visual.
+            PropertyMap pressMap = CreateImageVisual(pressImagePath);
+            button.SelectedVisual = pressMap;
+            button.UnselectedBackgroundVisual = normalMap;
+
+            // Chang background Visual and Label when focus gained.
+            button.FocusGained += (obj, e) =>
+            {
+                button.UnselectedBackgroundVisual = focusMap;
+                button.Label = _focusText;
+            };
+
+            // Chang background Visual and Label when focus lost.
+            button.FocusLost += (obj, e) =>
+            {
+                button.UnselectedBackgroundVisual = normalMap;
+                button.Label = _unfocusText;
+            };
+
+            // Chang background Visual when pressed.
+            button.KeyEvent += (obj, ee) =>
+            {
+                if ("Return" == ee.Key.KeyPressedName)
+                {
+                    if (Key.StateType.Down == ee.Key.State)
+                    {
+                        button.UnselectedBackgroundVisual = pressMap;
+                        Tizen.Log.Fatal("NUI", "Press in pushButton sample!!!!!!!!!!!!!!!!");
+                    }
+                    else if (Key.StateType.Up == ee.Key.State)
+                    {
+                        button.UnselectedBackgroundVisual = focusMap;
+                        Tizen.Log.Fatal("NUI", "Release in pushButton sample!!!!!!!!!!!!!!!!");
+                    }
+                }
+                return false;
+            };
+            return button;
+        }
+
+        /// <summary>
+        /// Create an Text visual.
+        /// </summary>
+        /// <param name="text">The text of the Text visual</param>
+        /// <param name="color">The color of the text</param>
+        private PropertyMap CreateTextVisual(string text, Color color)
+        {
+            PropertyMap map = new PropertyMap();
+            map.Add(Visual.Property.Type, new PropertyValue((int)Visual.Type.Text));
+            map.Add(TextVisualProperty.Text, new PropertyValue(text));
+            map.Add(TextVisualProperty.TextColor, new PropertyValue(color));
+            map.Add(TextVisualProperty.PointSize, new PropertyValue(8.0f));
+            map.Add(TextVisualProperty.HorizontalAlignment, new PropertyValue("CENTER"));
+            map.Add(TextVisualProperty.VerticalAlignment, new PropertyValue("CENTER"));
+            map.Add(TextVisualProperty.FontFamily, new PropertyValue("Samsung One 400"));
+            return map;
+        }
+
+        /// <summary>
+        /// Create an Image visual.
+        /// </summary>
+        /// <param name="imagePath">The url of the image</param>
+        private PropertyMap CreateImageVisual(string imagePath)
+        {
+            PropertyMap map = new PropertyMap();
+            map.Add(Visual.Property.Type, new PropertyValue((int)Visual.Type.Image));
+            map.Add(ImageVisualProperty.URL, new PropertyValue(imagePath));
+            return map;
+        }
+
+        /// <summary>
+        /// The event will be triggered when zoomInButton clicked
+        /// </summary>
+        /// <param name="source">zoomInButton.</param>
+        /// <param name="e">event</param>
+        /// <returns>the consume flag</returns>
+        private bool ZoomInButtonClick(object source, EventArgs e)
+        {
+            if (svgScale < 3.45)
+            {
+                svgScale *= 1.1f;
+                svgImage.Size2D = (new Vector2(200, 200)) * svgScale;
+            }
+            Tizen.Log.Fatal("NUI", "Print the svgScale: " + svgScale);
+            return true;
+        }
+
+        /// <summary>
+        /// The event will be triggered when zoomOutButton clicked
+        /// </summary>
+        /// <param name="source">zoomOutButton.</param>
+        /// <param name="e">event</param>
+        /// <returns>the consume flag</returns>
+        private bool ZoomOutButtonClick(object source, EventArgs e)
+        {
+            if(svgScale > 0.28)
+            {
+                svgScale /= 1.1f;
+                svgImage.Size2D = (new Vector2(200, 200)) * svgScale;
+            }
+            Tizen.Log.Fatal("NUI", "Print the svgScale: " + svgScale);
+            return true;
+        }
+    }
+}
