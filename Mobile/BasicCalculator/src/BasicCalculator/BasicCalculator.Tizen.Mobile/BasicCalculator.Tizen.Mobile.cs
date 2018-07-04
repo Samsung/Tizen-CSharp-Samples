b08eeac4ccb6a5f1754a88de3cb158a7fc3a3a48
Bartlomiej Uliasz <bartek@uliasz.net>
2018-07-04 13:21:15 +0200

    BasicCalculator - sample application update.
    Signed-off-by: Piotr Gorny <piotrek.gorny@gmail.com>

---
+++ Mobile/BasicCalculator/src/BasicCalculator/BasicCalculator.Tizen.Mobile/BasicCalculator.Tizen.Mobile.cs
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using ElmSharp;
+
+namespace BasicCalculator.Tizen.Mobile
+{
+    internal class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        #region methods
+
+        /// <summary>
+        /// Loads Xamarin application.
+        /// </summary>
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            MainWindow.AvailableRotations = DisplayRotation.Degree_0;
+            LoadApplication(new App());
+        }
+
+        /// <summary>
+        /// Main application entry point.
+        /// Initializes Xamarin Forms.
+        /// Initializes Tizen extensions.
+        /// </summary>
+        /// <param name="args">Application arguments.</param>
+        private static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+
+        #endregion
+    }
+}
---
+++ Mobile/BasicCalculator/src/BasicCalculator/BasicCalculator.Tizen.Mobile/Services/KeyboardService.cs
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using BasicCalculator.Tizen.Mobile.Services;
+using ElmSharp;
+using Xamarin.Forms.Platform.Tizen;
+using BasicCalculator.ViewModels;
+
+[assembly: Xamarin.Forms.Dependency(typeof(KeyboardService))]
+
+namespace BasicCalculator.Tizen.Mobile.Services
+{
+    /// <summary>
+    /// Keyboard listener and event dispatcher.
+    /// </summary>
+    internal class KeyboardService : IKeyboardService
+    {
+        #region fields
+
+        /// <summary>
+        /// Delegate used by <see cref="KeyEvent(object, EvasKeyEventArgs)"/> to maintain
+        /// Event registered methods.
+        /// </summary>
+        private KeyPressedDelegate _keyPressedDelegate;
+
+        #endregion fields
+
+        #region methods
+
+        /// <summary>
+        /// Event fired when any key is received by the application's MainWindow.
+        /// </summary>
+        /// <param name="sender">Sending object's reference.</param>
+        /// <param name="keyEventArgs">Key event arguments.</param>
+        private void KeyEvent(object sender, EvasKeyEventArgs keyEventArgs)
+        {
+            _keyPressedDelegate?.Invoke(keyEventArgs.KeyName);
+        }
+
+        /// <summary>
+        /// Registers KeyUp Event for keys passed as list in the first argument to execute
+        /// delegate passed in the second argument.
+        /// </summary>
+        /// <param name="keyList">List of keys names.</param>
+        /// <param name="keyPressedDelegate">Delegate to be executed on KeyUp event.</param>
+        public void RegisterKeys(string[] keyList, KeyPressedDelegate keyPressedDelegate)
+        {
+            _keyPressedDelegate += keyPressedDelegate;
+            Forms.Context.MainWindow.KeyUp += KeyEvent;
+            foreach (var key in keyList)
+            {
+                Forms.Context.MainWindow.KeyGrab(key, true);
+            }
+        }
+
+        #endregion methods
+    }
+}
