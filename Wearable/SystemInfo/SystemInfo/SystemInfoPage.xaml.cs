2dd8aff863ba596216ce8da6c19e06e5046b3a8a
Piotr Nowacki <p.nowacki2@samsung.com>
2018-08-06 06:46:31 +0200

    [Wearable][SystemInfo] New sample application

---
+++ Wearable/SystemInfo/SystemInfo/SystemInfoPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿//Copyright 2018 Samsung Electronics Co., Ltd
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+using Xamarin.Forms.Xaml;
+using Tizen.Wearable.CircularUI.Forms;
+using SystemInfo.InfoPages;
+namespace SystemInfo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SystemInfoPage : IndexPage
+    {
+
+        public SystemInfoPage()
+        {
+            InitializeComponent();
+            AddChildren();
+        }
+
+        /// <summary>
+        /// Add all the sub pages to the main page Children collection
+        /// </summary>    
+        private void AddChildren()
+        {
+            this.Children.Add(new BatteryPage());
+            this.Children.Add(new DisplayPage());
+            this.Children.Add(new CapabilitiesPage());
+            this.Children.Add(new SettingsPage());
+        }
+    }
+}
