08fc4ca50822934f92d6834166bd2f699038e791
Tomasz Zalewski <t.zalewski@samsung.com>
2018-08-14 13:37:13 +0200

    [Wearable][Accounts] New sample application

---
+++ Wearable/Accounts/Accounts/Accounts.cs
@@ -0,0 +1,45 @@
+//Copyright 2018 Samsung Electronics Co., Ltd
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+namespace Accounts
+{
+    /// <summary>
+    /// Represents Xamarin Forms of Tizen platform app.
+    /// </summary>
+    class Program : Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        /// <summary>
+        /// On create method.
+        /// </summary>
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            LoadApplication(new App());
+        }
+
+        /// <summary>
+        /// Main method of Accounts for Tizen Wearable.
+        /// </summary>
+        /// <param name="args"> Arguments. </param>
+        static void Main(string[] args)
+        {
+            var app = new Program();
+
+            Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            Tizen.Wearable.CircularUI.Forms.Renderer.FormsCircularUI.Init();
+            app.Run(args);
+        }
+    }
+}
---
+++ Wearable/Accounts/README.md
@@ -0,0 +1,17 @@
+# Tizen .NET Sample Account
+The Account application demonstrates how to implement an account provider which can add and configure an account.
+
+![Sign In Page](./screenshots/screenshot_1.png)
+![Sign Out Page](./screenshots/screenshot_2.png)
+![Settings](./screenshots/screenshot_3.png)
+
+### Features
+* Managing accounts using [Tizen.Account.AccountManager](https://developer.tizen.org/dev-guide/csapi/api/Tizen.Account.AccountManager.html)
+
+### Prerequisites
+
+* [Visual Studio](https://www.visualstudio.com/) - Buildtool, IDE
+* [Visual Studio Tools for Tizen](https://developer.tizen.org/development/visual-studio-tools-tizen/installing-visual-studio-tools-tizen) - Visual Studio plugin for Tizen .NET application development
+
+### Author
+* This application is a port of a [mobile app](/../../tree/master/Mobile/Accounts). The port was created by Tomasz Zalewski.
