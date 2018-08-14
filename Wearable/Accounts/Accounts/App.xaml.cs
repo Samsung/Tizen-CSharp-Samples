964bddb8059ae94db5c80145c76e04fd4221db27
Tomasz Zalewski <t.zalewski@samsung.com>
2018-08-14 13:37:13 +0200

    [Wearable][Accounts] New sample application

---
+++ Wearable/Accounts/Accounts/App.xaml.cs
@@ -0,0 +1,60 @@
+﻿//Copyright 2018 Samsung Electronics Co., Ltd
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+using Accounts.Views;
+using Xamarin.Forms;
+
+namespace Accounts
+{
+    /// <summary>
+    /// Sample Account Application Class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+
+            // Load main page.
+            MainPage = new NavigationPage(new AccountSignIn());
+        }
+
+        /// <summary>
+        /// On start method.
+        /// </summary>
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        /// <summary>
+        /// On sleep method.
+        /// </summary>
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        /// <summary>
+        /// On resume method.
+        /// </summary>
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
