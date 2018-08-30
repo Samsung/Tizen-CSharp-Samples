bf44a157b4b2beeebf148c6fb80e78a9357082d1
Juwon(Julia) Ahn <juwols@gmail.com>
2018-08-30 09:47:13 +0900

    [Wearable] Add WeatherWatch app

---
+++ Wearable/WeatherWatch/WeatherWatch/Pages/WeatherWatchPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://floralicense.org/license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tizen.Location;
+using WeatherWatch.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherWatch.Pages
+{
+    /// <summary>
+    /// WeatherWatchPage class
+    /// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WeatherWatchPage : ContentPage
+	{
+        public WeatherWatchPage()
+		{
+            InitializeComponent();
+        }
+
+        // Called when Battery Image or text is tapped
+        void Battery_Tapped(object sender, EventArgs e)
+        {
+            ViewModel.BatteryTextIsVisible = !ViewModel.BatteryTextIsVisible;
+        }
+
+        // Called when Air pollution Quality Index(AQI) Image or text is tapped
+        void AirPollution_Tapped(object sender, EventArgs e)
+        {
+            // Ignore when AQI Text information is not available
+            if (ViewModel.AqiText == "")
+            {
+                return;
+            }
+
+            ViewModel.AqiTextIsVisible = !ViewModel.AqiTextIsVisible;
+        }
+    }
+}
---
+++ Wearable/WeatherWatch/WeatherWatch/WeatherWatch.cs
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://floralicense.org/license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Tizen.Applications;
+using Tizen.Multimedia;
+using TSystem = Tizen.System;
+using Tizen.Wearable.CircularUI.Forms.Renderer.Watchface;
+using WeatherWatch.PageModels;
+using WeatherWatch.Pages;
+using Xamarin.Forms;
+using Tizen.System;
+using WeatherWatch.Services;
+
+namespace WeatherWatch
+{
+    /// <summary>
+    /// WeatherWatch app class
+    /// </summary>
+    class Program : FormsWatchface
+    {
+        WeatherWatchPageModel _viewModel;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Xamarin.Forms.Application xamarinApp = new Xamarin.Forms.Application();
+            TSystem.Display.StateChanged += Display_StateChanged;
+            xamarinApp.MainPage = new WeatherWatchPage();
+            _viewModel = (WeatherWatchPageModel)xamarinApp.MainPage.BindingContext;
+            LoadWatchface(xamarinApp);
+        }
+
+        // Raised when the screen of the device is on/off/dim.
+        private void Display_StateChanged(object sender, TSystem.DisplayStateChangedEventArgs e)
+        {
+            Console.WriteLine("[Display_StateChanged] " + e.State);
+            // When device's screen turns on
+            if (e.State == TSystem.DisplayState.Normal)
+            {
+                // Update Time for UI update
+                WatchTime time = GetCurrentTime();
+                _viewModel.Time = time.UtcTimestamp + TimeSpan.FromMilliseconds(time.Millisecond);
+                // Update Information about Weather & Battery
+                _viewModel.UpdateInformation();
+            }
+            else if (e.State == TSystem.DisplayState.Off)
+            {
+                // When the screen of the device is off
+                _viewModel.UnregisterEvents();
+            }
+        }
+
+        // Called when the time tick event comes.
+        // It's called when the screen of the device is on.
+        protected override void OnTick(TimeEventArgs time)
+        {
+            base.OnTick(time);
+            // Update Time for UI update
+            if (_viewModel != null)
+            {
+                _viewModel.Time = time.Time.UtcTimestamp + TimeSpan.FromMilliseconds(time.Time.Millisecond);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            // Initialize Tizen.Wearable.CircularUI.Forms
+            Tizen.Wearable.CircularUI.Forms.Renderer.FormsCircularUI.Init();
+            app.Run(args);
+        }
+    }
+}
