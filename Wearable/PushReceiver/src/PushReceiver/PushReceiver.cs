8672438d11d6d4a6f7af996ff54db892fcee94c4
Jakub Troc/C# API (SWP) /SRPOL/Associate/삼성전자 <j.troc@samsung.com>
2018-08-31 14:29:32 +0200

    [Wearable][PushReceiver] New sample application

---
+++ Wearable/PushReceiver/src/PushReceiver/PushReceiver.cs
@@ -0,0 +1,34 @@
+
+//Copyright 2018 Samsung Electronics Co., Ltd
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+namespace PushReceiver
+{
+    class Program : Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            Tizen.Wearable.CircularUI.Forms.Renderer.FormsCircularUI.Init();
+            app.Run(args);
+        }
+    }
+}
