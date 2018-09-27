f19a523b59dcdff07de94ae39d73c920eb1cd83a
Juwon(Julia) Ahn <juwols@gmail.com>
2018-09-27 12:15:14 +0900

    [Wearable][XamarinFormsAndServiceApps] new sample application

---
+++ Wearable/XamarinFormsAndServiceApps/ServiceApp/ServiceApp_App.cs
@@ -0,0 +1,60 @@
+﻿using Tizen.Applications;
+
+namespace ServiceApp
+{
+    class App : ServiceApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        protected override void OnAppControlReceived(AppControlReceivedEventArgs e)
+        {
+            base.OnAppControlReceived(e);
+            // Reply to a launch request
+            ReceivedAppControl receivedAppControl = e.ReceivedAppControl;
+            if (receivedAppControl.IsReplyRequest)
+            {
+                AppControl replyRequest = new AppControl();
+                receivedAppControl.ReplyToLaunchRequest(replyRequest, AppControlReplyResult.Succeeded);
+            }
+        }
+
+        protected override void OnDeviceOrientationChanged(DeviceOrientationEventArgs e)
+        {
+            base.OnDeviceOrientationChanged(e);
+        }
+
+        protected override void OnLocaleChanged(LocaleChangedEventArgs e)
+        {
+            base.OnLocaleChanged(e);
+        }
+
+        protected override void OnLowBattery(LowBatteryEventArgs e)
+        {
+            base.OnLowBattery(e);
+        }
+
+        protected override void OnLowMemory(LowMemoryEventArgs e)
+        {
+            base.OnLowMemory(e);
+        }
+
+        protected override void OnRegionFormatChanged(RegionFormatChangedEventArgs e)
+        {
+            base.OnRegionFormatChanged(e);
+        }
+
+        protected override void OnTerminate()
+        {
+            base.OnTerminate();
+        }
+
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.Run(args);
+        }
+    }
+}
---
+++ Wearable/XamarinFormsAndServiceApps/XamarinFormsAndServiceApps/App.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Tizen.Wearable.CircularUI.Forms;
+using Tizen.Applications;
+
+namespace XamarinFormsAndServiceApps
+{
+    public class App : Xamarin.Forms.Application
+    {
+        Label label;
+        public App()
+        {
+            label = new Label
+            {
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                Text = "Welcome to Xamarin Forms!"
+            };
+            // The root page of your application
+            MainPage = new CirclePage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        label
+                    }
+                }
+            };
+            LaunchServiceApp();
+        }
+
+        void LaunchServiceApp()
+        {
+            AppControl appcontrol = new AppControl
+            {
+                ApplicationId = "org.tizen.example.ServiceApp",
+                Operation = AppControlOperations.Default,
+            };
+
+            AppControl.SendLaunchRequest(appcontrol, (launchRequest, replyRequest, result) =>
+            {
+                switch (result)
+                {
+                    case AppControlReplyResult.Succeeded:
+                        label.Text = "Service application is successfully launched.";
+                        Console.WriteLine("Service application is successfully launched.");
+                        break;
+                    case AppControlReplyResult.Failed:
+                        label.Text = "Service application is not launched.";
+                        Console.WriteLine("Service application is not launched.");
+                        break;
+                    case AppControlReplyResult.AppStarted:
+                        label.Text = "Service application is just started.";
+                        Console.WriteLine("Service application is just started.");
+                        break;
+                    case AppControlReplyResult.Canceled:
+                        label.Text = "Service application is canceled.";
+                        Console.WriteLine("Service application is canceled.");
+                        break;
+                }
+            });
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
