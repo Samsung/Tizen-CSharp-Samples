951efba56a62f4041e31fa151ab6aa2002019e59
Seungkeun Lee <sngn.lee@samsung.com>
2018-10-08 09:07:16 +0900

    Add a new Wearable watch face sample

---
+++ Wearable/PhotoWatch/PhotoWatch/BackgroundImageModel.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace PhotoWatch
+{
+    public class BackgroundImageModel
+    {
+        public string Path { get; set; }
+        public string DisplayName { get; set; }
+        public Command<string> DeleteCommand { get; set; }
+    }
+}
---
+++ Wearable/PhotoWatch/PhotoWatch/SettingView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Tizen.Wearable.CircularUI.Forms;
+
+namespace PhotoWatch
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingView : ContentView
+    {
+        public SettingView ()
+        {
+            InitializeComponent();
+        }
+
+        public IRotaryFocusable RotaryFocusObject => BgList;
+    }
+}
