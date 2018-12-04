e7f1475ef3cd33729a8f099e74074d2b7df52a47
pr.jung <pr.jung@samsung.com>
2018-12-04 20:11:14 +0900

    [Wearable][StorageApp][Add sample application for Tizen.System.Storage]
    Signed-off-by: pr.jung <pr.jung@samsung.com>

---
+++ Wearable/StorageApp/StorageApp/StorageApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tizen.Wearable.CircularUI.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Tizen.System;
+
+namespace StorageApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : CirclePage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnClicked(object sender, EventArgs args)
+        {
+            var storages = StorageManager.Storages;
+
+            foreach (Storage storage in storages)
+            {
+                if (storage.StorageType != StorageArea.Internal)
+                    continue;
+                this.Navigation.PushAsync(new StoragePage(storage.Id, storage.RootDirectory, storage.TotalSpace, storage.AvailableSpace, storage.State));
+            }
+        }
+    }
+}
---
+++ Wearable/StorageApp/StorageApp/StorageApp/StoragePrint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorageApp
+{
+    class StoragePrint
+    {
+        public string Detail { get; set; }
+        
+        public StoragePrint(string str)
+        {
+            Detail = str;
+        }
+    }
+}
