c00a43985f165683c422c1ea5191b75c8554b385
Juwon(Julia) Ahn <juwols@gmail.com>
2019-04-19 17:58:11 +0900

    [Wearable][WidgetSample] Add the feature to show how to use WebProxy.
     - When tapping on "Get Data" button, it requests to receive https data packets.

---
+++ Wearable/WidgetSample/src/WidgetSample/Utility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WidgetSample
+{
+    public static class Utility
+    {
+        public const string InternetPrivilege = "http://tizen.org/privilege/internet";
+        public const string LOG_TAG = "MyWidget";
+    }
+}
