d4460fc6fa85abeb039730cb56cd9b74a76fff41
Sunggyu Choi <sunggyu.choi@samsung.com>
2019-06-12 14:57:31 +0900

    Add new script to build apps with CLI tool
    Signed-off-by: Sunggyu Choi <sunggyu.choi@samsung.com>

---
+++ TV/build_tv.sh
@@ -0,0 +1,137 @@
+cd ScriptLayoutSample 
+sudo dotnet build ScriptLayoutSample.sln
+cd ..
+
+cd MapView/src
+sudo dotnet build Maps.sln
+cd ../..
+
+cd SystemInfo/src/
+sudo dotnet build SystemInfo.sln
+cd ../..
+
+cd ImageSample
+sudo dotnet build ImageSample.sln
+cd ..
+
+cd MusicPlayer/src/
+sudo dotnet build MusicPlayer.sln
+cd ../..
+
+cd FlexContainerSample
+sudo dotnet build FlexContainerSample.sln
+cd ..
+
+cd Ball3D
+sudo dotnet build Ball3D.sln
+cd ..
+
+cd MediaContent/src/
+sudo dotnet build MediaContent.sln
+cd ../..
+
+cd ScratchPaper
+sudo dotnet build ScratchPaper.sln
+cd ..
+
+cd Stopwatch/src/
+sudo dotnet build Stopwatch.sln
+cd ../..
+
+cd ChannelList
+sudo dotnet build ChannelList.sln
+cd ..
+
+cd BasicCalculator/src
+sudo dotnet build BasicCalculator.sln
+cd ../..
+
+cd Triangle
+sudo dotnet build Triangle.sln
+cd ..
+
+cd Graffiti
+sudo dotnet build Graffiti.sln
+cd ..
+
+cd CubeTexture
+sudo dotnet build CubeTexture.sln
+cd ..
+
+cd MediaHubSample
+sudo dotnet build MediaHubSample.sln
+cd ..
+
+cd Star
+sudo dotnet build Star.sln
+cd ..
+
+cd Preference/src
+sudo dotnet build Preference.sln
+cd ../..
+
+cd CalendarComponent/src
+sudo dotnet build CalendarComponent.sln
+cd ..
+
+cd Panorama360
+sudo dotnet build Panorama360.sln
+cd ..
+
+cd CubeWithSkiaSharp
+sudo dotnet build CubeWithSkiaSharp.sln
+cd ..
+
+cd AppInfo/src
+sudo dotnet build AppInfo.sln
+cd ..
+
+cd VisualSample
+sudo dotnet build VisualSample.sln
+cd ..
+
+cd MusicPlayerUI
+sudo dotnet build MusicPlayerUI.sln
+cd ..
+
+cd SecureRepository
+sudo dotnet build SecureRepository.sln
+cd ..
+
+cd FirstScreen
+sudo dotnet build FirstScreen.sln
+cd ..
+
+cd UIControlSample
+sudo dotnet build UIControlSample.sln
+cd ..
+
+cd Gallery
+sudo dotnet build Gallery.sln
+cd ..
+
+cd MimeTypeConverter/src
+sudo dotnet build MimeTypeConverter.sln
+cd ../..
+
+cd AnimationSample
+sudo dotnet build AnimationSample.sln
+cd ..
+
+cd TextReader/src
+sudo dotnet build TextReader.sln
+cd ../..
+
+cd ParticleDynamic
+sudo dotnet build ParticleDynamic.sln
+cd ..
+
+cd ErrorCodeConverter/src
+sudo dotnet build ErrorCodeConverter.sln
+cd ../..
+
+cd TextSample
+sudo dotnet build TextSample.sln
+cd ..
+
+
---
+++ Wearable/build_w.sh
@@ -0,0 +1,305 @@
+cd BadgeCounter/src
+sudo dotnet build BadgeCounter.sln
+cd ../..
+
+cd SystemInfo2/SystemInfo2/SystemInfo2
+sudo dotnet build SystemInfo2.sln
+cd ../../..
+
+cd TonePlayerSample
+sudo dotnet build TonePlayerSample.sln
+cd ..
+
+cd ScriptLayoutSample
+sudo dotnet build ScriptLayoutSample.sln
+cd ..
+
+cd QRCodeGenerator
+sudo dotnet build QRCodeGenerator.sln
+cd ..
+
+cd AppHistory
+sudo dotnet build AppHistory.sln
+cd ..
+
+cd VolumeController/src
+sudo dotnet build VolumeController.sln
+cd ../..
+
+cd AmbientWatch
+sudo dotnet build AmbientWatch.sln
+cd ..
+
+cd XStopWatch
+sudo dotnet build XStopWatch.sln
+cd ..
+
+cd SystemInfo/src
+sudo dotnet build SystemInfo.sln
+cd ../..
+
+cd XamarinFormsAndServiceApps
+sudo dotnet build XamarinFormsAndServiceApps.sln
+cd ..
+
+cd PushReceiver/src
+sudo dotnet build PushReceiver.sln
+cd ../..
+
+cd SQLite.NET.Sample
+sudo dotnet build SQLite.NET.Sample.sln
+cd ..
+
+cd ImageSample
+sudo dotnet build ImageSample.sln
+cd ..
+
+cd Compass/src
+sudo dotnet build Compass.sln
+cd ../..
+
+cd PlayingWithHWInputs
+sudo dotnet build PlayingWithHWInputs.sln
+cd ..
+
+cd ClassicWatch
+sudo dotnet build ClassicWatch.sln
+cd ..
+
+cd CustomFonts/src/NUIFontTest
+sudo dotnet build NUIFontTest.sln
+cd ../../..
+
+cd CustomFonts/src/WorkingWithFonts
+sudo dotnet build WorkingWithFonts.sln
+cd ../../..
+
+cd TextFieldSample
+sudo dotnet build TextFieldSample.sln
+cd ..
+
+cd SpeechToText
+sudo dotnet build SpeechToText.sln
+cd ..
+
+cd FeedbackApp/src
+sudo dotnet build FeedbackApp.sln
+cd ..
+
+cd DeviceApp
+sudo dotnet build DeviceApp.sln
+cd ..
+
+cd MediaContent
+sudo dotnet build MediaContent.sln
+cd ..
+
+cd PhotoWatch
+sudo dotnet build PhotoWatch.sln
+cd ..
+
+cd Badges/src
+sudo dotnet build Badges.sln
+cd ../..
+
+cd SampleTelephony
+sudo dotnet build SampleTelephony.sln
+cd ..
+
+cd MessagePortSampleApp
+sudo dotnet build MessagePortSampleApp.sln
+cd ..
+
+cd AppInformation
+sudo dotnet build AppInformation.sln
+cd ..
+
+cd WavPlayerSample
+sudo dotnet build WavPlayerSample.sln
+cd ..
+
+cd VisualsSample
+sudo dotnet build VisualsSample.sln
+cd ..
+
+cd HeartRateMonitor/src
+sudo dotnet build HeartRateMonitor.sln
+cd ../..
+
+cd ImageViewer
+sudo dotnet build ImageViewer.sln
+cd ..
+
+cd MetalDetector/src
+sudo dotnet build MetalDetector.sln
+cd ../..
+
+cd Lescanner
+sudo dotnet build Lescanner.sln
+cd ..
+
+cd ChronographWatch
+sudo dotnet build ChronographWatch.sln
+cd ..
+
+cd Calculator/src
+sudo dotnet build Calculator.sln
+cd ../..
+
+cd TextLabelSample
+sudo dotnet build TextLabelSample.sln
+cd ..
+
+cd NFCSampleApp
+sudo dotnet build NFCSampleApp.sln
+cd ..
+
+cd WeatherWatch
+sudo dotnet build WeatherWatch.sln
+cd ..
+
+cd Maps
+sudo dotnet build Maps.sln
+cd ..
+
+cd Sensors/src
+sudo dotnet build Sensors.sln
+cd ../..
+
+cd WidgetSample/src
+sudo dotnet build WidgetSample.sln
+cd ../..
+
+cd WidgetSample/UIandWidget
+sudo dotnet build UIApp.sln
+cd ../..
+
+cd AppStatistics/src
+sudo dotnet build AppStatistics.sln
+cd ../..
+
+cd Accounts
+sudo dotnet build Accounts.sln
+cd ..
+
+cd Preference
+sudo dotnet build Preference.sln
+cd ..
+
+cd Downloader/src
+sudo dotnet build Downloader.sln
+cd ../..
+
+cd ImageReader
+sudo dotnet build ImageReader.sln
+cd ..
+
+cd Geocoding/src
+sudo dotnet build Geocoding.sln
+cd ../..
+
+cd ReverseGeocoding/src
+sudo dotnet build ReverseGeocoding.sln
+cd ../..
+
+cd Location
+sudo dotnet build Location.sln
+cd ..
+
+cd IMESample
+sudo dotnet build IMESample.sln
+cd ..
+
+cd AnalogWatch/src
+sudo dotnet build AnalogWatch.sln
+cd ../..
+
+cd StorageApp
+sudo dotnet build StorageApp.sln
+cd ..
+
+cd SecureRepository
+sudo dotnet build SecureRepository.sln
+cd ..
+
+cd VoiceMemo
+sudo dotnet build VoiceMemo.sln
+cd ..
+
+cd ServiceApp
+sudo dotnet build ServiceApp.sln
+cd ..
+
+cd UIControlSample
+sudo dotnet build UIControlSample.sln
+cd ..
+
+cd AppControl
+sudo dotnet build AppControl.sln
+cd ..
+
+cd CircularUIMediaPlayer/src
+sudo dotnet build CircularUIMediaPlayer.sln
+cd ../..
+
+cd XUIComponents
+sudo dotnet build XUIComponents.sln
+cd ..
+
+cd MapsView/src
+sudo dotnet build MapsView.sln
+cd ../..
+
+cd Weather
+sudo dotnet build Weather.sln
+cd ..
+
+cd TextEditorSample
+sudo dotnet build TextEditorSample.sln
+cd ..
+
+cd SkiaSharp2DSample
+sudo dotnet build SkiaSharp2DSample.sln
+cd ..
+
+cd WebWidgetHybrid
+sudo dotnet build WebWidgetHybrid.sln
+cd ..
+
+cd NetworkApp
+sudo dotnet build NetworkApp.sln
+cd ..
+
+cd TextReader
+sudo dotnet build TextReader.sln
+cd ..
+
+cd IMEManager
+sudo dotnet build IMEManager.sln
+cd ..
+
+cd UsingResxLocalization
+sudo dotnet build UsingResxLocalization.sln
+cd ..
+
+cd Alarm
+sudo dotnet build Alarm.sln
+cd ..
+
+cd ServiceDiscovery/src
+sudo dotnet build ServiceDiscovery.sln
+cd ../..
+
+cd SmartCardSampleApp
+sudo dotnet build SmartCardSampleApp.sln
+cd ..
+
+cd RotaryTimer/src
+sudo dotnet build RotaryTimer.sln
+cd ../..
+
+cd AnimationsSample
+sudo dotnet build AnimationsSample.sln
+cd ..
+
+
