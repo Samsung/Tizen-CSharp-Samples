5e3e56468b88291beadfc959be5619b3498cebcf
Sunggyu Choi <sunggyu.choi@samsung.com>
2019-06-14 17:00:13 +0900

    Fix path error when running CLI build
    Signed-off-by: Sunggyu Choi <sunggyu.choi@samsung.com>

--- TV/build_tv.sh
+++ TV/build_tv.sh
@@ -1,137 +1,137 @@
+echo "-----------------------------------------------------------------------------------"
 cd ScriptLayoutSample 
 sudo dotnet build ScriptLayoutSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd MapView/src
 sudo dotnet build Maps.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd SystemInfo/src/
 sudo dotnet build SystemInfo.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd ImageSample
 sudo dotnet build ImageSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd MusicPlayer/src/
 sudo dotnet build MusicPlayer.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd FlexContainerSample
 sudo dotnet build FlexContainerSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Ball3D
 sudo dotnet build Ball3D.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd MediaContent/src/
 sudo dotnet build MediaContent.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd ScratchPaper
 sudo dotnet build ScratchPaper.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Stopwatch/src/
 sudo dotnet build Stopwatch.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd ChannelList
 sudo dotnet build ChannelList.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd BasicCalculator/src
 sudo dotnet build BasicCalculator.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd Triangle
 sudo dotnet build Triangle.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Graffiti
 sudo dotnet build Graffiti.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd CubeTexture
 sudo dotnet build CubeTexture.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd MediaHubSample
 sudo dotnet build MediaHubSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Star
 sudo dotnet build Star.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Preference/src
 sudo dotnet build Preference.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd CalendarComponent/src
 sudo dotnet build CalendarComponent.sln
-cd ..
-
+cd ../..
+echo "-----------------------------------------------------------------------------------"
 cd Panorama360
 sudo dotnet build Panorama360.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd CubeWithSkiaSharp
 sudo dotnet build CubeWithSkiaSharp.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd AppInfo/src
 sudo dotnet build AppInfo.sln
-cd ..
-
+cd ../..
+echo "-----------------------------------------------------------------------------------"
 cd VisualSample
 sudo dotnet build VisualSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd MusicPlayerUI
 sudo dotnet build MusicPlayerUI.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd SecureRepository
 sudo dotnet build SecureRepository.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd FirstScreen
 sudo dotnet build FirstScreen.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd UIControlSample
 sudo dotnet build UIControlSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Gallery
 sudo dotnet build Gallery.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd MimeTypeConverter/src
 sudo dotnet build MimeTypeConverter.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd AnimationSample
 sudo dotnet build AnimationSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd TextReader/src
 sudo dotnet build TextReader.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd ParticleDynamic
 sudo dotnet build ParticleDynamic.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd ErrorCodeConverter/src
 sudo dotnet build ErrorCodeConverter.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd TextSample
 sudo dotnet build TextSample.sln
 cd ..
-
 
--- Wearable/build_w.sh
+++ Wearable/build_w.sh
@@ -1,305 +1,305 @@
+echo "-----------------------------------------------------------------------------------"
 cd BadgeCounter/src
 sudo dotnet build BadgeCounter.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd SystemInfo2/SystemInfo2/SystemInfo2
 sudo dotnet build SystemInfo2.sln
 cd ../../..
-
+echo "-----------------------------------------------------------------------------------"
 cd TonePlayerSample
 sudo dotnet build TonePlayerSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd ScriptLayoutSample
 sudo dotnet build ScriptLayoutSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd QRCodeGenerator
 sudo dotnet build QRCodeGenerator.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd AppHistory
 sudo dotnet build AppHistory.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd VolumeController/src
 sudo dotnet build VolumeController.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd AmbientWatch
 sudo dotnet build AmbientWatch.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd XStopWatch
 sudo dotnet build XStopWatch.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd SystemInfo/src
 sudo dotnet build SystemInfo.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd XamarinFormsAndServiceApps
 sudo dotnet build XamarinFormsAndServiceApps.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd PushReceiver/src
 sudo dotnet build PushReceiver.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd SQLite.NET.Sample
 sudo dotnet build SQLite.NET.Sample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd ImageSample
 sudo dotnet build ImageSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Compass/src
 sudo dotnet build Compass.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd PlayingWithHWInputs
 sudo dotnet build PlayingWithHWInputs.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd ClassicWatch
 sudo dotnet build ClassicWatch.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd CustomFonts/src/NUIFontTest
 sudo dotnet build NUIFontTest.sln
 cd ../../..
-
+echo "-----------------------------------------------------------------------------------"
 cd CustomFonts/src/WorkingWithFonts
 sudo dotnet build WorkingWithFonts.sln
 cd ../../..
-
+echo "-----------------------------------------------------------------------------------"
 cd TextFieldSample
 sudo dotnet build TextFieldSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd SpeechToText
 sudo dotnet build SpeechToText.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd FeedbackApp/src
 sudo dotnet build FeedbackApp.sln
-cd ..
-
+cd ../..
+echo "-----------------------------------------------------------------------------------"
 cd DeviceApp
 sudo dotnet build DeviceApp.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd MediaContent
 sudo dotnet build MediaContent.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd PhotoWatch
 sudo dotnet build PhotoWatch.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Badges/src
 sudo dotnet build Badges.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd SampleTelephony
 sudo dotnet build SampleTelephony.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd MessagePortSampleApp
 sudo dotnet build MessagePortSampleApp.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd AppInformation
 sudo dotnet build AppInformation.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd WavPlayerSample
 sudo dotnet build WavPlayerSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd VisualsSample
 sudo dotnet build VisualsSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd HeartRateMonitor/src
 sudo dotnet build HeartRateMonitor.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd ImageViewer
 sudo dotnet build ImageViewer.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd MetalDetector/src
 sudo dotnet build MetalDetector.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd Lescanner
 sudo dotnet build Lescanner.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd ChronographWatch
 sudo dotnet build ChronographWatch.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Calculator/src
 sudo dotnet build Calculator.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd TextLabelSample
 sudo dotnet build TextLabelSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd NFCSampleApp
 sudo dotnet build NFCSampleApp.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd WeatherWatch
 sudo dotnet build WeatherWatch.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Maps
 sudo dotnet build Maps.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Sensors/src
 sudo dotnet build Sensors.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd WidgetSample/src
 sudo dotnet build WidgetSample.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd WidgetSample/UIandWidget
 sudo dotnet build UIApp.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd AppStatistics/src
 sudo dotnet build AppStatistics.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd Accounts
 sudo dotnet build Accounts.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Preference
 sudo dotnet build Preference.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Downloader/src
 sudo dotnet build Downloader.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd ImageReader
 sudo dotnet build ImageReader.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Geocoding/src
 sudo dotnet build Geocoding.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd ReverseGeocoding/src
 sudo dotnet build ReverseGeocoding.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd Location
 sudo dotnet build Location.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd IMESample
 sudo dotnet build IMESample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd AnalogWatch/src
 sudo dotnet build AnalogWatch.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd StorageApp
 sudo dotnet build StorageApp.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd SecureRepository
 sudo dotnet build SecureRepository.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd VoiceMemo
 sudo dotnet build VoiceMemo.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd ServiceApp
 sudo dotnet build ServiceApp.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd UIControlSample
 sudo dotnet build UIControlSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd AppControl
 sudo dotnet build AppControl.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd CircularUIMediaPlayer/src
 sudo dotnet build CircularUIMediaPlayer.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd XUIComponents
 sudo dotnet build XUIComponents.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd MapsView/src
 sudo dotnet build MapsView.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd Weather
 sudo dotnet build Weather.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd TextEditorSample
 sudo dotnet build TextEditorSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd SkiaSharp2DSample
 sudo dotnet build SkiaSharp2DSample.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd WebWidgetHybrid
 sudo dotnet build WebWidgetHybrid.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd NetworkApp
 sudo dotnet build NetworkApp.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd TextReader
 sudo dotnet build TextReader.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd IMEManager
 sudo dotnet build IMEManager.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd UsingResxLocalization
 sudo dotnet build UsingResxLocalization.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd Alarm
 sudo dotnet build Alarm.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd ServiceDiscovery/src
 sudo dotnet build ServiceDiscovery.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd SmartCardSampleApp
 sudo dotnet build SmartCardSampleApp.sln
 cd ..
-
+echo "-----------------------------------------------------------------------------------"
 cd RotaryTimer/src
 sudo dotnet build RotaryTimer.sln
 cd ../..
-
+echo "-----------------------------------------------------------------------------------"
 cd AnimationsSample
 sudo dotnet build AnimationsSample.sln
 cd ..
-
 
