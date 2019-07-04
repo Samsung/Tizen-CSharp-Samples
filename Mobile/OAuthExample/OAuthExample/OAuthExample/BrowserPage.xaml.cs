0ded27269059169e94e021ca1b6c689f85d08091
Seungkeun Lee <sngn.lee@samsung.com>
2019-07-04 19:46:57 +0900

    Fix indent

--- Mobile/OAuthExample/OAuthExample/OAuthExample/BrowserPage.xaml.cs
+++ Mobile/OAuthExample/OAuthExample/OAuthExample/BrowserPage.xaml.cs
@@ -4,17 +4,17 @@ using Xamarin.Forms.Xaml;
 namespace OAuthExample
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class BrowserPage : ContentPage
-	{
-		public BrowserPage(string url)
-		{
-			InitializeComponent();
+    public partial class BrowserPage : ContentPage
+    {
+        public BrowserPage(string url)
+        {
+            InitializeComponent();
             ToolbarItems.Add(new ToolbarItem("Cancel", null, () =>
             {
                 Navigation.PopAsync();
             }, ToolbarItemOrder.Secondary));
             browser.Source = url;
-		}
+        }
         protected override bool OnBackButtonPressed()
         {
             return true;
