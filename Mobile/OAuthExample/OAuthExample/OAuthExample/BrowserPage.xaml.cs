ad1d4ac1ca0923c061abb16f62b22db1905799f9
Seungkeun Lee <sngn.lee@samsung.com>
2019-07-04 19:38:21 +0900

    Add OAuth Authorization example

---
+++ Mobile/OAuthExample/OAuthExample/OAuthExample/BrowserPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OAuthExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BrowserPage : ContentPage
+	{
+		public BrowserPage(string url)
+		{
+			InitializeComponent();
+            ToolbarItems.Add(new ToolbarItem("Cancel", null, () =>
+            {
+                Navigation.PopAsync();
+            }, ToolbarItemOrder.Secondary));
+            browser.Source = url;
+		}
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
