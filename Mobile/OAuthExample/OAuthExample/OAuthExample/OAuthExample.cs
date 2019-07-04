9ee39d3f91ef168f197dba5b912d9d400346c809
Seungkeun Lee <sngn.lee@samsung.com>
2019-07-04 19:38:21 +0900

    Add OAuth Authorization example

---
+++ Mobile/OAuthExample/OAuthExample/OAuthExample/OAuthExample.cs
@@ -0,0 +1,28 @@
+using Xamarin.Forms;
+
+namespace OAuthExample
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
