669cec180d90d263ddb781ac1f2b094f277bdeac
gichan-jang <gichan2.jang@samsung.com>
2020-02-25 14:19:09 +0900

    [sample] Add text classification sample Add text classification sample app with NNStreamer single-API
    Signed-off-by: gichan-jang <gichan2.jang@samsung.com>

---
+++ Mobile/MachineLearning/TextClassification/TextClassification/TextClassification/TextClassification.xaml.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using TextClassification.Views;
+
+namespace TextClassification
+{
+    /// <summary>
+    /// Text Classification Application Class
+    /// </summary>
+
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes a new instance of the App class.
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+
+            /* Load main page. */
+            MainPage = new NavigationPage(new UserInput());
+        }
+
+        /// <summary>
+        /// On start method
+        /// </summary>
+        protected override void OnStart()
+        {
+            /// Handle when your app starts
+        }
+
+        /// <summary>
+        /// On sleep method
+        /// </summary>
+        protected override void OnSleep()
+        {
+            /* Handle when your app sleeps */
+        }
+
+        /// <summary>
+        /// On resume method
+        /// </summary>
+        protected override void OnResume()
+        {
+            /// Handle when your app resumes
+        }
+    }
+}
---
+++ Mobile/MachineLearning/TextClassification/TextClassification/TextClassification/Views/UserInput.xaml.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Threading;
+using Log = Tizen.Log;
+
+/// <summary>
+/// Show User input page
+/// </summary>
+namespace TextClassification.Views
+{
+    /// <summary>
+    /// Content page of User input
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserInput : ContentPage
+    {
+        const string TAG = "TextClassification.Views.UserInput";
+        private string Comment;
+        private HandleText TextModel = new HandleText();
+
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public UserInput()
+        {
+            InitializeComponent();
+
+            var entryComment = new Entry();
+
+            CommentChange(entryComment);
+
+        }
+
+        /// <summary>
+        /// Event when "Get result" button is clicked.
+        /// </summary>
+        /// <param name="sender">Instance of the object which invokes event.</param>
+        /// <param name="args"> Event argument.</param>
+        public async void CreateClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            InputLabel.Text = Comment;
+            float[] input;
+            float[] output = new float[2];
+
+            /// Initialize the model
+            TextModel.Init_model();
+            /// Convert user input to float array
+            input = TextModel.SplitStr(Comment);
+
+            if (string.IsNullOrEmpty(Comment))
+            {
+                Log.Warn(TAG, "User input is empty");
+                /// Check wrong Input parameter
+                await DisplayAlert("Error!", "Please enter your comment", "OK");
+            }
+            else if (Equals(button.Text, "Get Result"))
+            {
+                /// Invoke the model
+                API.Text = "Single-shot";
+                output = TextModel.Invoke_mode(input);
+            }
+            else
+            {
+                Log.Error(TAG, "Unknown button is pressed");
+            }
+
+            /// Show text classification resut
+            if (output[0].Equals(0) && output[1].Equals(0))
+            {
+                Out_neg.Text = "Invalid result";
+                Out_pos.Text = "InValid result";
+            }
+            else
+            {
+                Out_neg.Text = output[0].ToString();
+                Out_pos.Text = output[1].ToString();
+            }
+        }
+
+        /// <summary>
+        /// Event when H/W back button is clicked.
+        /// </summary>
+        /// <returns> true if exit is successful </returns>
+        protected override bool OnBackButtonPressed()
+        {
+            System.Environment.Exit(1);
+            return true;
+        }
+
+        /// <summary>
+        ///  Entry object get comment from user input
+        /// </summary>
+        /// <param name="entryComment"> User comment </param>
+        private void CommentChange(Entry entryComment) => entryComment.TextChanged += UserComment;
+
+        /// <summary>
+        /// Set user comment received from user input event
+        /// </summary>
+        /// <param name="sender"> Parameter about which object is invoked the current event. </param>
+        /// <param name="e"> Event data of comment changed </param>
+        private void UserComment(object sender, TextChangedEventArgs e) => Comment = e.NewTextValue;
+    }
+}
