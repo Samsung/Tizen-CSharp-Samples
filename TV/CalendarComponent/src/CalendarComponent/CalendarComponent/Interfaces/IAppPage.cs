613fdab87d3e9695b7dd5d956e8143802d3f8d7c
Patryk Falba <p.falba@samsung.com>
2020-05-18 16:05:15 +0200

    Remove CalendarComponent(TV)
    Signed-off-by: Patryk Falba <p.falba@samsung.com>

--- TV/CalendarComponent/src/CalendarComponent/CalendarComponent/Interfaces/IAppPage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CalendarComponent.Interfaces
-{
-    /// <summary>
-    /// Interface for obtaining project's view using Dependency Injection.
-    /// </summary>
-    public interface IAppPage
-    {
-    }
-}
