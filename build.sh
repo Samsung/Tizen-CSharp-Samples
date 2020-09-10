da1111bbf7372c6b17c36b4502f104bdf8b64e18
Sunggyu Choi <sunggyu.choi@samsung.com>
2020-09-10 17:51:07 +0900

    Update build.sh file to be used for ahub
    Signed-off-by: Sunggyu Choi <sunggyu.choi@samsung.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-rm build_result.txt
-
 for file in $(find ./ -name "*.sln"); do
-    echo "$file"
-    sudo dotnet clean "./$file"
-    sudo dotnet clean "---------------------------------------------------------------------------------------" >> build_result.txt
-    sudo dotnet build "./$file" >> build_result.txt
+    dotnet build "./$file"
 done
