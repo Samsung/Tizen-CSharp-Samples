7cd2430d51efe4d2b253cfe61ac16d5c6d4a8b49
Michal Szczecinski <mszczecinsk@partner.samsung.com>
2020-11-12 09:50:04 +0100

    [Mobile][NUI][CustomView] Added custom view sample.
    This sample applicaiton code was used to enhance documentation for
    Custom View interface. This example don't contain any json code. Only
    minimum set of functions and visuals was used to implement view from
    screenshot.

---
+++ Mobile/NUI_CustomView/NUI_CustomView/ContactView.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace NUI_CustomView
+{
+    public class ContactView : CustomView
+    {
+        private string name;
+        private string resDir;
+
+        private const int BASE_INDEX = 10000;
+        private const int BACKGROUND_VISUAL_INDEX = BASE_INDEX + 1;
+        private const int LABEL_VISUAL_INDEX = BASE_INDEX + 2;
+        private const int CONTACT_BG_ICON_INDEX = BASE_INDEX + 3;
+        private const int CONTACT_ICON_INDEX = BASE_INDEX + 4;
+        private const int CONTACT_EDIT_INDEX = BASE_INDEX + 5;
+        private const int CONTACT_FAVORITE_INDEX = BASE_INDEX + 6;
+        private const int CONTACT_DELETE_INDEX = BASE_INDEX + 7;
+
+        static ContactView()
+        {
+            CustomViewRegistry.Instance.Register(CreateInstance, typeof(ContactView));
+        }
+
+        static CustomView CreateInstance()
+        {
+            return new ContactView(null, null);
+        }
+
+        private void CreateBackground(Vector4 color)
+        {
+            PropertyMap map = new PropertyMap();
+            map.Add(Visual.Property.Type, new PropertyValue((int)Visual.Type.Color))
+               .Add(ColorVisualProperty.MixColor, new PropertyValue(color));
+
+            VisualBase background = VisualFactory.Instance.CreateVisual(map);
+            RegisterVisual(BACKGROUND_VISUAL_INDEX, background);
+            background.DepthIndex = BACKGROUND_VISUAL_INDEX;
+        }
+
+        private void CreateLabel(string text)
+        {
+            PropertyMap textVisual = new PropertyMap();
+            textVisual.Add(Visual.Property.Type, new PropertyValue((int)Visual.Type.Text))
+                      .Add(TextVisualProperty.Text, new PropertyValue(text))
+                      .Add(TextVisualProperty.TextColor, new PropertyValue(Color.Black))
+                      .Add(TextVisualProperty.PointSize, new PropertyValue(12))
+                      .Add(TextVisualProperty.HorizontalAlignment, new PropertyValue("CENTER"))
+                      .Add(TextVisualProperty.VerticalAlignment, new PropertyValue("CENTER"));
+
+            VisualBase label = VisualFactory.Instance.CreateVisual(textVisual);
+            RegisterVisual(LABEL_VISUAL_INDEX, label);
+            label.DepthIndex = LABEL_VISUAL_INDEX;
+
+            PropertyMap imageVisualTransform = new PropertyMap();
+            imageVisualTransform.Add((int)VisualTransformPropertyType.Offset, new PropertyValue(new Vector2(30, 5)))
+                                .Add((int)VisualTransformPropertyType.OffsetPolicy, new PropertyValue(new Vector2((int)VisualTransformPolicyType.Absolute, (int)VisualTransformPolicyType.Absolute)))
+                                .Add((int)VisualTransformPropertyType.SizePolicy, new PropertyValue(new Vector2((int)VisualTransformPolicyType.Absolute, (int)VisualTransformPolicyType.Absolute)))
+                                .Add((int)VisualTransformPropertyType.Size, new PropertyValue(new Vector2(350, 100)));
+
+            label.SetTransformAndSize(imageVisualTransform, new Vector2(this.SizeWidth, this.SizeHeight));
+        }
+
+        private void CreateIcon(string url, float x, float y, float w, float h, int index)
+        {
+            PropertyMap map = new PropertyMap();
+            PropertyMap transformMap = new PropertyMap();
+
+            map.Add(Visual.Property.Type, new PropertyValue((int)Visual.Type.Image))
+               .Add(ImageVisualProperty.URL, new PropertyValue(url));
+
+            VisualBase icon = VisualFactory.Instance.CreateVisual(map);
+
+            PropertyMap imageVisualTransform = new PropertyMap();
+            imageVisualTransform.Add((int)VisualTransformPropertyType.Offset, new PropertyValue(new Vector2(x, y)))
+                                .Add((int)VisualTransformPropertyType.OffsetPolicy, new PropertyValue(new Vector2((int)VisualTransformPolicyType.Absolute, (int)VisualTransformPolicyType.Absolute)))
+                                .Add((int)VisualTransformPropertyType.SizePolicy, new PropertyValue(new Vector2((int)VisualTransformPolicyType.Absolute, (int)VisualTransformPolicyType.Absolute)))
+                                .Add((int)VisualTransformPropertyType.Size, new PropertyValue(new Vector2(w, h)))
+                                .Add((int)VisualTransformPropertyType.Origin, new PropertyValue((int)Visual.AlignType.CenterBegin))
+                                .Add((int)VisualTransformPropertyType.AnchorPoint, new PropertyValue((int)Visual.AlignType.CenterBegin));
+
+            icon.SetTransformAndSize(imageVisualTransform, new Vector2(this.SizeWidth, this.SizeHeight));
+
+            RegisterVisual(index, icon);
+            icon.DepthIndex = index;
+        }
+
+        public ContactView(string name, string resDir) : base(typeof(ContactView).Name, CustomViewBehaviour.ViewBehaviourDefault)
+        {
+            this.name = name;
+            this.resDir = resDir;
+
+            CreateBackground(new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+            CreateIcon(resDir + "/images/cbg.png", 10.0f, 5.0f, 100.0f, 100.0f, CONTACT_BG_ICON_INDEX);
+            CreateIcon(resDir + "/images/contact.png", 10.0f, 5.0f, 100.0f, 100.0f, CONTACT_ICON_INDEX);
+            CreateIcon(resDir + "/images/edit.png", 130.0f, 40.0f, 50.0f, 50.0f, CONTACT_EDIT_INDEX);
+            CreateIcon(resDir + "/images/favorite.png", 180.0f, 40.0f, 50.0f, 50.0f, CONTACT_FAVORITE_INDEX);
+            CreateIcon(resDir + "/images/delete.png", 640.0f, 40.0f, 50.0f, 50.0f, CONTACT_DELETE_INDEX);
+            CreateLabel(name);
+        }
+    }
+}
---
+++ Mobile/NUI_CustomView/NUI_CustomView/NUI_CustomView.cs
@@ -0,0 +1,55 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace NUI_CustomView
+{
+    class Program : NUIApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            Window window = Window.Instance;
+            window.KeyEvent += OnKeyEvent;
+
+            ImageView background = new ImageView(DirectoryInfo.Resource + "/images/bg.png");
+            background.Size2D = new Size2D(window.Size.Width, window.Size.Height);
+            window.Add(background);
+]
+            View mainView = new View();
+
+            LinearLayout ly = new LinearLayout();
+            ly.LinearOrientation = LinearLayout.Orientation.Vertical;
+            ly.CellPadding = new Size2D(0, 13);
+            ly.Padding = new Extents(10, 10, 10, 10);
+
+            mainView.Layout = ly;
+            window.Add(mainView);
+
+            for (int i = 0; i < 9; i++)
+            {
+                NUI_CustomView.ContactView contactView = new ContactView("Test: " + i.ToString(), DirectoryInfo.Resource);
+                contactView.Size2D = new Size2D(window.Size.Width - 20, window.Size.Height / 10);
+                mainView.Add(contactView);
+            }
+        }
+
+        public void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
