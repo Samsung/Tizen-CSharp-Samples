fb6319015bfe6e495eb699e9113636d5619bc076
Jakub Ociepa <j.ociepa@samsung.com>
2021-01-04 15:12:22 +0100

    [Wearable] Upgrade Tizen version in QRCodeGenerator sample app.

--- Wearable/QRCodeGenerator/QRCodeGenerator/QRCodeGenerator.Tizen.Wearable/QRCodeGenerator.Tizen.Wearable.cs
+++ Wearable/QRCodeGenerator/QRCodeGenerator/QRCodeGenerator.Tizen.Wearable/QRCodeGenerator.Tizen.Wearable.cs
@@ -43,7 +43,7 @@ namespace QRCodeGenerator
         {
             var app = new Program();
             Forms.Init(app);
-            global::Tizen.Wearable.CircularUI.Forms.Renderer.FormsCircularUI.Init();
+            global::Tizen.Wearable.CircularUI.Forms.FormsCircularUI.Init();
             app.Run(args);
         }
 
