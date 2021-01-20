599baf4b2452c9df277af2b857bb616c59e40bb9
p.kalota <p.kalota@samsung.com>
2021-01-20 13:31:53 +0100

    [Mobile] VideoViewSample: Apply review comments

--- Mobile/VideoViewSample/VideoViewSample/VideoViewSample.cs
+++ Mobile/VideoViewSample/VideoViewSample/VideoViewSample.cs
@@ -1,4 +1,20 @@
-﻿using Tizen.NUI;
+﻿/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Tizen.NUI;
 using Tizen.NUI.Components;
 using Tizen.NUI.BaseComponents;
 
@@ -6,7 +22,7 @@ namespace VideoViewSample
 {
     class Program : NUIApplication
     {
-        private readonly string[] btnNames = { "play", "pause", "stop", "forward", "backward" };
+        private readonly string[] buttonNames = { "play", "pause", "stop", "forward", "backward" };
         private VideoView player;
 
         protected override void OnCreate()
@@ -29,7 +45,7 @@ namespace VideoViewSample
             player.Play();
             Window.Instance.Add(player);
 
-            for (int i = 0; i < btnNames.Length; ++i)
+            for (int i = 0; i < buttonNames.Length; ++i)
             {
                 int buttonPosX = (int)player.SizeHeight + 50 + i * 102;
                 Button btn = new Button
@@ -39,7 +55,7 @@ namespace VideoViewSample
                     Position2D = new Position2D(-150, buttonPosX),
                     BackgroundColor = new Color("#00bfff"),
                     TextColor = Color.White,
-                    Text = btnNames[i],
+                    Text = buttonNames[i],
                 };
                 btn.Clicked += OnClicked;
                 Window.Instance.Add(btn);
