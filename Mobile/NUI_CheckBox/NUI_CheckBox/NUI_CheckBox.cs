b3caa7343a99bd3b3a5eafe006902f781d51f5a8
mgrudzinska <m.grudzinska@samsung.com>
2021-01-24 01:31:48 +0100

    [Mobile][NUI] CheckBox example added - applying PascalCase and adding copyright comments

--- Mobile/NUI_CheckBox/NUI_CheckBox/NUI_CheckBox.cs
+++ Mobile/NUI_CheckBox/NUI_CheckBox/NUI_CheckBox.cs
@@ -1,50 +1,85 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
 using Tizen.NUI;
 using Tizen.NUI.Components;
 using Tizen.NUI.BaseComponents;
 
 namespace NUI_CheckBox
 {
-    // Custom style of the checkbox
+    /// <summary>
+    /// Custom style of the checkbox
+    /// </summary>
     internal class CustomCheckBoxStyle : StyleBase
     {
-        string _URL = Tizen.Applications.Application.Current.DirectoryInfo.Resource + "images/";
-        private Size2D _size = new Size2D(200,200);
+        string ImageURL = Tizen.Applications.Application.Current.DirectoryInfo.Resource + "images/";
+        private Size2D CheckBoxSize = new Size2D(200,200);
 
         protected override ViewStyle GetViewStyle()
         {
-            ButtonStyle style = new ButtonStyle
+            ButtonStyle Style = new ButtonStyle
             {
-                // Square area connected with click event
-                Size = _size,
+                /// Square area connected with click event
+                Size = CheckBoxSize,
                 Icon = new ImageViewStyle
                 {
-                    Size = _size,
+                    Size = CheckBoxSize,
                     ResourceUrl = new Selector<string>
                     {
                         Other = "",
-                        Selected = _URL + "RoundCheckMark.png"
+                        Selected = ImageURL + "RoundCheckMark.png"
                     },
-                    // Round shadow, but the area connected with click event remains square
-                    ImageShadow = new ImageShadow(_URL + "RoundShadow.png"),
+                    /// Round shadow, but the area connected with click event remains square
+                    ImageShadow = new ImageShadow(ImageURL + "RoundShadow.png"),
                 },
                 ParentOrigin = ParentOrigin.BottomCenter,
                 PositionUsesPivotPoint = true,
                 PivotPoint = PivotPoint.BottomCenter
             };
-            return style;
+            return Style;
         }
     }
 
     class Program : NUIApplication
     {
-        // Path to the directory with images
-        static string _URL = Tizen.Applications.Application.Current.DirectoryInfo.Resource + "images/";
-        private View _view;
-        private CheckBox _checkBox;
-        private CheckBox _customCheckBox;
-        private Size2D _checkBoxSize = new Size2D(200,200);
-        private int _space = 50;
+        /// <summary>
+        /// Path to the directory with images
+        /// </summary>
+        static string ImageURL = Tizen.Applications.Application.Current.DirectoryInfo.Resource + "images/";
+        /// <summary>
+        /// View with all CheckBoxes
+        /// </summary>
+        private View MainView;
+        /// <summary>
+        /// Used for a default CheckBox and CheckBox created with properties
+        /// </summary>
+        private CheckBox CheckBoxExample;
+        /// <summary>
+        /// Used for a custom style CheckBox
+        /// </summary>
+        private CheckBox CustomCheckBox;
+        /// <summary>
+        /// Size of each CheckBox
+        /// </summary>
+        private Size2D CheckBoxSize = new Size2D(200,200);
+        /// <summary>
+        /// Space between CheckBoxes inside the MainView
+        /// </summary>
+        private int Space = 50;
 
         protected override void OnCreate()
         {
@@ -54,111 +89,124 @@ namespace NUI_CheckBox
 
         void Initialize()
         {
-            Window.Instance.KeyEvent += Window_KeyEvent;
+            Window.Instance.KeyEvent += WindowKeyEvent;
 
-            _view = new View();
-            _view.Size = new Size2D(_checkBoxSize.Width + 100, 4 * _checkBoxSize.Height + 5 * _space);
-            _view.PivotPoint = PivotPoint.Center;
-            _view.ParentOrigin = ParentOrigin.Center;
-            _view.PositionUsesPivotPoint = true;
-            _view.BackgroundColor = Color.White;
-            Window.Instance.Add(_view);
+            MainView = new View();
+            MainView.Size = new Size2D(CheckBoxSize.Width + 100, 4 * CheckBoxSize.Height + 5 * Space);
+            MainView.PivotPoint = PivotPoint.Center;
+            MainView.ParentOrigin = ParentOrigin.Center;
+            MainView.PositionUsesPivotPoint = true;
+            MainView.BackgroundColor = Color.White;
+            Window.Instance.Add(MainView);
 
             // Default CheckBox
-            _checkBox = new CheckBox();
-            _checkBox.Size = _checkBoxSize;
-            _checkBox.ParentOrigin = ParentOrigin.TopCenter;
-            _checkBox.PositionUsesPivotPoint = true;
-            _checkBox.PivotPoint = PivotPoint.TopCenter;
-            _checkBox.Position = new Position(0, _space);
-            _view.Add(_checkBox);
+            CheckBoxExample = new CheckBox();
+            CheckBoxExample.Size = CheckBoxSize;
+            CheckBoxExample.ParentOrigin = ParentOrigin.TopCenter;
+            CheckBoxExample.PositionUsesPivotPoint = true;
+            CheckBoxExample.PivotPoint = PivotPoint.TopCenter;
+            CheckBoxExample.Position = new Position(0, Space);
+            MainView.Add(CheckBoxExample);
 
             // Create with properties
-            _checkBox = new CheckBox();
-            _checkBox.Size = _checkBoxSize;
-            _checkBox.ParentOrigin = ParentOrigin.Center;
-            _checkBox.PositionUsesPivotPoint = true;
-            _checkBox.PivotPoint = PivotPoint.Center;
-            _checkBox.Position = new Position(0, - (_checkBoxSize.Height + _space) / 2);
-            // Set the icon images for different check box states 
-            StringSelector _iconURL = new StringSelector()
+            CheckBoxExample = new CheckBox();
+            CheckBoxExample.Size = CheckBoxSize;
+            CheckBoxExample.ParentOrigin = ParentOrigin.Center;
+            CheckBoxExample.PositionUsesPivotPoint = true;
+            CheckBoxExample.PivotPoint = PivotPoint.Center;
+            CheckBoxExample.Position = new Position(0, - (CheckBoxSize.Height + Space) / 2);
+            // Set the icon images for different check box states
+            StringSelector IconURL = new StringSelector()
             {
-                Normal           = _URL + "Blue.png",
-                Selected         = _URL + "BlueCheckMark.png",
-                Pressed          = _URL + "Red.png",
+                Normal           = ImageURL + "Blue.png",
+                Selected         = ImageURL + "BlueCheckMark.png",
+                Pressed          = ImageURL + "Red.png",
             };
-            _checkBox.IconURLSelector = _iconURL;
-            _checkBox.Icon.Size = new Size2D(160,160);
-            _checkBox.BackgroundColor = new Color(0.57f, 0.7f, 1.0f, 0.8f);
+            CheckBoxExample.IconURLSelector = IconURL;
+            CheckBoxExample.Icon.Size = new Size2D(160,160);
+            CheckBoxExample.BackgroundColor = new Color(0.57f, 0.7f, 1.0f, 0.8f);
             // CheckBox initial state set to be selected
-            _checkBox.IsSelected = true;
+            CheckBoxExample.IsSelected = true;
             // CheckBox can be selected
-            _checkBox.IsSelectable = true;
+            CheckBoxExample.IsSelectable = true;
             // CheckBox is enabled
-            _checkBox.IsEnabled = true;
-            _view.Add(_checkBox);
+            CheckBoxExample.IsEnabled = true;
+            MainView.Add(CheckBoxExample);
 
             // Create with style - since the CheckBox inherits after the Button class, the ButtonStyle is used
-            ButtonStyle _style = new ButtonStyle
+            ButtonStyle Style = new ButtonStyle
             {
                 IsSelectable = true,
                 ParentOrigin = ParentOrigin.Center,
                 PositionUsesPivotPoint = true,
                 PivotPoint = PivotPoint.Center,
-                Position = new Position(0, (_checkBoxSize.Height + _space) / 2),
-                Size = _checkBoxSize,
+                Position = new Position(0, (CheckBoxSize.Height + Space) / 2),
+                Size = CheckBoxSize,
                 // Gray structural background
-                BackgroundImage = _URL + "Struct.png",
+                BackgroundImage = ImageURL + "Struct.png",
                 // Image overlaid on the background
                 Icon = new ImageViewStyle
                 {
-                    Size =  _checkBoxSize,
+                    Size =  CheckBoxSize,
                     // Different icon used depending on the status
                     ResourceUrl = new Selector<string>
                     {
-                        Other = _URL + "XSign.png",
-                        Selected = _URL + "CheckMark.png"
+                        Other = ImageURL + "XSign.png",
+                        Selected = ImageURL + "CheckMark.png"
                     },
                     // Icon opacity set to 0.8 for all checkbox states
                     Opacity = 0.8f,
                     // Shadow visible for all states
-                    ImageShadow = new ImageShadow(_URL + "Shadow.png")
+                    ImageShadow = new ImageShadow(ImageURL + "Shadow.png")
                 },
                 // Style of the overlay image
                 Overlay = new ImageViewStyle()
                 {
                     ResourceUrl = new Selector<string>
                     {
-                        Pressed = _URL + "Red.png",
-                        Other   = _URL + "LightBlue.png"
+                        Pressed = ImageURL + "Red.png",
+                        Other   = ImageURL + "LightBlue.png"
                     },
                     Opacity = new Selector<float?> {Pressed = 0.3f, Other = 1.0f}
                 }
             };
-            _checkBox = new CheckBox(_style);
-            _view.Add(_checkBox);
+            CheckBoxExample = new CheckBox(Style);
+            MainView.Add(CheckBoxExample);
 
             // Create with custom style
             // Custom style registration
             Tizen.NUI.Components.StyleManager.Instance.RegisterStyle("_CustomCheckBoxStyle", null, typeof(NUI_CheckBox.CustomCheckBoxStyle));
-            _customCheckBox = new CheckBox("_CustomCheckBoxStyle");
-            _customCheckBox.Position = new Position(0, -_space);
+            CustomCheckBox = new CheckBox("_CustomCheckBoxStyle");
+            CustomCheckBox.Position = new Position(0, -Space);
             // Click event handle
-            _customCheckBox.Clicked += OnClicked;
-            _view.Add(_customCheckBox);
+            CustomCheckBox.Clicked += OnClicked;
+            MainView.Add(CustomCheckBox);
         }
 
-        // Called after clicking the checkbox
+        /// <summary>
+        /// Event handler after the CheckBox is clicked
+        /// </summary>
+        /// <param name="sender"> Event sender </param>
+        /// <param name="e"> Event argument </param>
         private void OnClicked(object sender, EventArgs e)
         {
-            if (_customCheckBox.IsSelected)
-                _view.BackgroundColor = Color.Green;
+            if (CustomCheckBox.IsSelected)
+            {
+                MainView.BackgroundColor = Color.Green;
+            }
             else
-                _view.BackgroundColor = Color.White;
+            {
+                MainView.BackgroundColor = Color.White;
+            }
         }
 
-        // Responds on pressing chosen buttons
-        private void Window_KeyEvent(object sender, Window.KeyEventArgs e)
+        /// <summary>
+        /// Called when the chosen key event is received.
+        /// Use to exit the application
+        /// </summary>
+        /// <param name="sender"> Event sender </param>
+        /// <param name="e"> Event argument </param>
+        private void WindowKeyEvent(object sender, Window.KeyEventArgs e)
         {
             if (e.Key.State == Key.StateType.Down)
             {
@@ -169,11 +217,15 @@ namespace NUI_CheckBox
             }
         }
 
+        /// <summary>
+        /// Entry method of the program/application.
+        /// </summary>
+        /// <param name="args">Launch arguments.</param>
         static void Main(string[] args)
         {
-            var app = new Program();
-            app.Run(args);
-            app.Dispose();
+            var App = new Program();
+            App.Run(args);
+            App.Dispose();
         }
     }
 }
--- Mobile/NUI_CheckBox/README.md
+++ Mobile/NUI_CheckBox/README.md
@@ -2,11 +2,14 @@
 
 **This sample application demonstrates how to use NUI CheckBox.**
 
-Checkbox is a UI component connected with the click events. 
+Checkbox is a UI component connected with the click events.
 The base class for a `CheckBox` class is a `Button`, which properties can be used to specify the checkbox.
 
-The sample application reacts to the click event, after which it changes the background color. 
+The sample application reacts to the click event, after which it changes the background color.
 The animation illustrating the app behavior is as follows:
-| [anim](!./../NUI_CheckBox.gif) |
+
+![anim](./NUI_CheckBox.gif)
+![screen1](./NUI_CheckBox_screen1.png)
+![screen2](./NUI_CheckBox_screen2.png)
 
 For more information about the checkbox see the [NUI guide](https://docs.tizen.org/application/dotnet/guides/nui/nui-components/CheckBox/).
