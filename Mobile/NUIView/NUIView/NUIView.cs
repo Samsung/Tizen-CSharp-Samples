3968f421c70a2427766d5ffdbd834868c3c2d32a
Mateusz Palkowski <37028026+MathewP1@users.noreply.github.com>
2021-02-09 11:24:33 +0100

    Added license and deleted commented lines

--- Mobile/NUIView/NUIView/NUIView.cs
+++ Mobile/NUIView/NUIView/NUIView.cs
@@ -1,4 +1,20 @@
-﻿using System;
+/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
 using Tizen.NUI;
 using Tizen.NUI.Components;
 using Tizen.NUI.BaseComponents;
@@ -52,13 +68,9 @@ namespace NUIView
             _mainView.SetSizeModeFactor(new Vector3(0.0f, 0.7f, 0.0f));
 
             _textLabel = new TextLabel();
-            // _textLabel.BackgroundColor = new Color(0.2f, 0.2f, 0.2f, 1.0f);
             _textLabel.PositionUsesPivotPoint = true;
             _textLabel.PivotPoint = PivotPoint.TopCenter; 
             _textLabel.ParentOrigin = new Vector3(0.5f, 0.1f, 0.0f);
-            // _textLabel.WidthResizePolicy = ResizePolicyType.FillToParent;
-            // _textLabel.HeightResizePolicy = ResizePolicyType.SizeRelativeToParent;
-            // _textLabel.SetSizeModeFactor(new Vector3(0.0f, 0.2f, 0.0f));
             _textLabel.MultiLine = true;
             _textLabel.TextColor = Color.White;
 
