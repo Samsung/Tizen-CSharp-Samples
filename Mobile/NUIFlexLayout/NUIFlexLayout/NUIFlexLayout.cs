fe187b4ef442c34fe93c1eb9518c1d1816e93a73
Mateusz Palkowski <m.palkowski@samsung.com>
2021-02-10 10:47:29 +0100

    added comments

--- Mobile/NUIFlexLayout/NUIFlexLayout/NUIFlexLayout.cs
+++ Mobile/NUIFlexLayout/NUIFlexLayout/NUIFlexLayout.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright (c) 2021 Samsung Electronics Co., Ltd All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the License);
@@ -23,16 +23,30 @@ namespace NUIFlexLayout
 {
     class Program : NUIApplication
     {
-        private View _rootView, _topView, _buttonView, _flexView;
-        private Button _button1, _button2, _button3, _button4;
-        private static int _itemsCnt = 20;
+        /// <summary>
+        /// All views present in the application
+        /// </summary>
+        private View RootView, TopView, ButtonView, FlexView;
+        /// <summary>
+        /// Buttons used to change properties of flex view
+        /// </summary>
+        private Button Button1, Button2, Button3, Button4;
+        /// <summary>
+        /// Number of items used in
+        private static int ItemsCnt = 20;
 
+        /// <summary>
+        /// Overriden method that is called on app creation
+        /// </summary>
         protected override void OnCreate()
         {
             base.OnCreate();
             Initialize();
         }
 
+        /// <summary>
+        /// Initialize all views, layouts and buttons used in application
+        /// </summary>
         void Initialize()
         {
             InitViews();
@@ -40,156 +54,188 @@ namespace NUIFlexLayout
             InitButtons();
         }
 
+        /// <summary>
+        /// Initialize all views
+        /// </summary>
         private void InitViews()
         {
-            Size2D screenSize = Window.Instance.WindowSize;
+            RootView = new View();
+            RootView.PositionUsesPivotPoint = true;
+            RootView.PivotPoint = PivotPoint.Center;
+            RootView.ParentOrigin = ParentOrigin.Center;
+            RootView.WidthResizePolicy = ResizePolicyType.FillToParent;
+            RootView.HeightResizePolicy = ResizePolicyType.FillToParent;
+            RootView.BackgroundColor = Color.Black;
 
-            _rootView = new View();
-            _rootView.PositionUsesPivotPoint = true;
-            _rootView.PivotPoint = PivotPoint.Center;
-            _rootView.ParentOrigin = ParentOrigin.Center;
-            _rootView.WidthResizePolicy = ResizePolicyType.FillToParent;
-            _rootView.HeightResizePolicy = ResizePolicyType.FillToParent;
-            _rootView.BackgroundColor = Color.Black;
+            TopView = new View();
+            TopView.PositionUsesPivotPoint = true;
+            TopView.PivotPoint = PivotPoint.TopCenter;
+            TopView.ParentOrigin = new Vector3(0.5f, 0.05f, 0.0f);
+            TopView.WidthResizePolicy = ResizePolicyType.FillToParent;
+            TopView.HeightResizePolicy = ResizePolicyType.SizeRelativeToParent;
+            TopView.SetSizeModeFactor(new Vector3(0.0f, 0.7f, 0.0f));
+            TopView.Padding = new Extents(20, 20, 20, 20);
+            TopView.BackgroundColor = Color.Black;
 
-            _topView = new View();
-            _topView.PositionUsesPivotPoint = true;
-            _topView.PivotPoint = PivotPoint.TopCenter;
-            _topView.ParentOrigin = new Vector3(0.5f, 0.05f, 0.0f);
-            _topView.WidthResizePolicy = ResizePolicyType.FillToParent;
-            _topView.HeightResizePolicy = ResizePolicyType.SizeRelativeToParent;
-            _topView.SetSizeModeFactor(new Vector3(0.0f, 0.7f, 0.0f));
-            _topView.Padding = new Extents(20, 20, 20, 20);
-            _topView.BackgroundColor = Color.Black;
+            ButtonView = new View();
+            ButtonView.PositionUsesPivotPoint = true;
+            ButtonView.PivotPoint = PivotPoint.TopCenter;
+            ButtonView.ParentOrigin = new Vector3(0.5f, 0.85f, 0.0f);
+            ButtonView.WidthResizePolicy = ResizePolicyType.FillToParent;
+            ButtonView.HeightResizePolicy = ResizePolicyType.SizeRelativeToParent;
+            ButtonView.SetSizeModeFactor(new Vector3(0.0f, 0.2f, 0.0f));
+            ButtonView.Padding = new Extents(20, 20, 20, 20);
+            ButtonView.BackgroundColor = Color.Black;
 
-            _buttonView = new View();
-            _buttonView.PositionUsesPivotPoint = true;
-            _buttonView.PivotPoint = PivotPoint.TopCenter;
-            _buttonView.ParentOrigin = new Vector3(0.5f, 0.85f, 0.0f);
-            _buttonView.WidthResizePolicy = ResizePolicyType.FillToParent;
-            _buttonView.HeightResizePolicy = ResizePolicyType.SizeRelativeToParent;
-            _buttonView.SetSizeModeFactor(new Vector3(0.0f, 0.2f, 0.0f));
-            _buttonView.Padding = new Extents(20, 20, 20, 20);
-            _buttonView.BackgroundColor = Color.Black;
-
-            _rootView.Add(_topView);
-            _rootView.Add(_buttonView);
-            Window.Instance.Add(_rootView);
+            RootView.Add(TopView);
+            RootView.Add(ButtonView);
+            Window.Instance.Add(RootView);
             InitFlex();
             Window.Instance.KeyEvent += OnKeyEvent;
         }
 
+        /// <summary>
+        /// Initialize flex layout and fill it with text labels
+        /// </summary>
         private void InitFlex()
         {
-            _flexView = new View();
-            _flexView.PositionUsesPivotPoint = true;
-            _flexView.PivotPoint = PivotPoint.Center;
-            _flexView.ParentOrigin = ParentOrigin.Center;
-            _flexView.HeightSpecification = LayoutParamPolicies.MatchParent;
-            _flexView.WidthSpecification = LayoutParamPolicies.MatchParent;
-            _flexView.BackgroundColor = Color.Black;
-            _flexView.Layout = new FlexLayout() {WrapType = FlexLayout.FlexWrapType.Wrap};
-            _topView.Add(_flexView);
+            FlexView = new View();
+            FlexView.PositionUsesPivotPoint = true;
+            FlexView.PivotPoint = PivotPoint.Center;
+            FlexView.ParentOrigin = ParentOrigin.Center;
+            FlexView.HeightSpecification = LayoutParamPolicies.MatchParent;
+            FlexView.WidthSpecification = LayoutParamPolicies.MatchParent;
+            FlexView.BackgroundColor = Color.Black;
+            FlexView.Layout = new FlexLayout() {WrapType = FlexLayout.FlexWrapType.Wrap};
+            TopView.Add(FlexView);
 
-            for (int i = 0; i < _itemsCnt; i++)
+            for (int i = 0; i < ItemsCnt; i++)
             {
                 TextLabel t = new TextLabel();
                 t.Margin = new Extents(10, 10, 10, 10);
                 t.Text = "X " + i;
                 t.BackgroundColor = new Color(0.8f, 0.1f, 0.2f, 1.0f);
-                _flexView.Add(t);
+                FlexView.Add(t);
             }
         }
 
+        /// <summary>
+        /// Initialize buttons that change flex layout properties
+        /// </summary>
         private void InitButtons()
         {
-            LinearLayout buttonLayout = new LinearLayout();
-            buttonLayout.LinearOrientation = LinearLayout.Orientation.Horizontal;
-            _buttonView.Layout = buttonLayout;
+            LinearLayout ButtonLayout = new LinearLayout();
+            ButtonLayout.LinearOrientation = LinearLayout.Orientation.Horizontal;
+            ButtonView.Layout = ButtonLayout;
 
-            _button1 = new Button();
-            _button1.BackgroundColor = new Color(0.25f, 0.25f, 0.25f, 1.0f);
-            _button1.HeightResizePolicy = ResizePolicyType.FillToParent;
-            _button1.Text = "Dir";
-            _button1.TextColor = Color.White;
-            _button1.Margin = new Extents(10, 10, 10, 10);
-            _button1.Weight = 0.25f;
+            Button1 = new Button();
+            Button1.BackgroundColor = new Color(0.25f, 0.25f, 0.25f, 1.0f);
+            Button1.HeightResizePolicy = ResizePolicyType.FillToParent;
+            Button1.Text = "Dir";
+            Button1.TextColor = Color.White;
+            Button1.Margin = new Extents(10, 10, 10, 10);
+            Button1.Weight = 0.25f;
 
-            _button2 = new Button();
-            _button2.BackgroundColor = new Color(0.25f, 0.25f, 0.25f, 1.0f);
-            _button2.HeightResizePolicy = ResizePolicyType.FillToParent;
-            _button2.Text = "Just";
-            _button2.TextColor = Color.White;
-            _button2.Margin = new Extents(10, 10, 10, 10);
-            _button2.Weight = 0.25f;
+            Button2 = new Button();
+            Button2.BackgroundColor = new Color(0.25f, 0.25f, 0.25f, 1.0f);
+            Button2.HeightResizePolicy = ResizePolicyType.FillToParent;
+            Button2.Text = "Just";
+            Button2.TextColor = Color.White;
+            Button2.Margin = new Extents(10, 10, 10, 10);
+            Button2.Weight = 0.25f;
 
-            _button3 = new Button();
-            _button3.BackgroundColor = new Color(0.25f, 0.25f, 0.25f, 1.0f);
-            _button3.HeightResizePolicy = ResizePolicyType.FillToParent;
-            _button3.Text = "Align";
-            _button3.TextColor = Color.White;
-            _button3.Margin = new Extents(10, 10, 10, 10);
-            _button3.Weight = 0.25f;
+            Button3 = new Button();
+            Button3.BackgroundColor = new Color(0.25f, 0.25f, 0.25f, 1.0f);
+            Button3.HeightResizePolicy = ResizePolicyType.FillToParent;
+            Button3.Text = "Align";
+            Button3.TextColor = Color.White;
+            Button3.Margin = new Extents(10, 10, 10, 10);
+            Button3.Weight = 0.25f;
 
-            _button4 = new Button();
-            _button4.BackgroundColor = new Color(0.25f, 0.25f, 0.25f, 1.0f);
-            _button4.HeightResizePolicy = ResizePolicyType.FillToParent;
-            _button4.Text = "Wrap";
-            _button4.TextColor = Color.White;
-            _button4.Margin = new Extents(10, 10, 10, 10);
-            _button4.Weight = 0.25f;
+            Button4 = new Button();
+            Button4.BackgroundColor = new Color(0.25f, 0.25f, 0.25f, 1.0f);
+            Button4.HeightResizePolicy = ResizePolicyType.FillToParent;
+            Button4.Text = "Wrap";
+            Button4.TextColor = Color.White;
+            Button4.Margin = new Extents(10, 10, 10, 10);
+            Button4.Weight = 0.25f;
 
-            _buttonView.Add(_button1);
-            _buttonView.Add(_button2);
-            _buttonView.Add(_button3);
-            _buttonView.Add(_button4);
+            ButtonView.Add(Button1);
+            ButtonView.Add(Button2);
+            ButtonView.Add(Button3);
+            ButtonView.Add(Button4);
 
-            _button1.ClickEvent += Button1Clicked;
-            _button2.ClickEvent += Button2Clicked;
-            _button3.ClickEvent += Button3Clicked;
-            _button4.ClickEvent += Button4Clicked;
+            Button1.ClickEvent += Button1Clicked;
+            Button2.ClickEvent += Button2Clicked;
+            Button3.ClickEvent += Button3Clicked;
+            Button4.ClickEvent += Button4Clicked;
         }
 
+        /// <summary>
+        /// The method called when the button 1 is clicked
+        /// </summary>
+        /// <param name="sender">Button instance</param>
+        /// <param name="e">Event arguments</param>
         private void Button1Clicked(object sender, Button.ClickEventArgs e)
         {
-            FlexLayout layout = (FlexLayout)_flexView.Layout;
-            FlexLayout.FlexDirection newDirection = layout.Direction + 1;
+            FlexLayout Layout = (FlexLayout)FlexView.Layout;
+            FlexLayout.FlexDirection newDirection = Layout.Direction + 1;
             if (newDirection > FlexLayout.FlexDirection.RowReverse)
                 newDirection = FlexLayout.FlexDirection.Column;
-            layout.Direction = newDirection;
-            _flexView.Layout = layout;
+            Layout.Direction = newDirection;
+            FlexView.Layout = Layout;
         }
 
+        /// <summary>
+        /// The method called when the button 2 is clicked
+        /// </summary>
+        /// <param name="sender">Button instance</param>
+        /// <param name="e">Event arguments</param>
         private void Button2Clicked(object sender, Button.ClickEventArgs e)
         {
-            FlexLayout layout = (FlexLayout)_flexView.Layout;
-            FlexLayout.FlexJustification newJust = layout.Justification + 1;
+            FlexLayout Layout = (FlexLayout)FlexView.Layout;
+            FlexLayout.FlexJustification newJust = Layout.Justification + 1;
             if (newJust > FlexLayout.FlexJustification.SpaceAround)
                 newJust = FlexLayout.FlexJustification.FlexStart;
-            layout.Justification = newJust;
-            _flexView.Layout = layout;
+            Layout.Justification = newJust;
+            FlexView.Layout = Layout;
         }
 
+        /// <summary>
+        /// The method called when the button 3 is clicked
+        /// </summary>
+        /// <param name="sender">Button instance</param>
+        /// <param name="e">Event arguments</param>
         private void Button3Clicked(object sender, Button.ClickEventArgs e)
         {
-            FlexLayout layout = (FlexLayout)_flexView.Layout;
-            FlexLayout.AlignmentType newAlign = layout.Alignment + 1;
+            FlexLayout Layout = (FlexLayout)FlexView.Layout;
+            FlexLayout.AlignmentType newAlign = Layout.Alignment + 1;
             if (newAlign > FlexLayout.AlignmentType.Stretch)
                 newAlign = FlexLayout.AlignmentType.Auto;
-            layout.Alignment = newAlign;
-            _flexView.Layout = layout;
+            Layout.Alignment = newAlign;
+            FlexView.Layout = Layout;
         }
 
+        /// <summary>
+        /// The method called when the button 4 is clicked
+        /// </summary>
+        /// <param name="sender">Button instance</param>
+        /// <param name="e">Event arguments</param>
         private void Button4Clicked(object sender, Button.ClickEventArgs e)
         {
-            FlexLayout layout = (FlexLayout)_flexView.Layout;
-            if (layout.WrapType == FlexLayout.FlexWrapType.Wrap)
-                layout.WrapType = FlexLayout.FlexWrapType.NoWrap;
+            FlexLayout Layout = (FlexLayout)FlexView.Layout;
+            if (Layout.WrapType == FlexLayout.FlexWrapType.Wrap)
+                Layout.WrapType = FlexLayout.FlexWrapType.NoWrap;
             else
-                layout.WrapType = FlexLayout.FlexWrapType.Wrap;
-            _flexView.Layout = layout;
+                Layout.WrapType = FlexLayout.FlexWrapType.Wrap;
+            FlexView.Layout = Layout;
         }
 
+        /// <summary>
+        /// Method which is called when key event happens
+        /// </summary>
+        /// <param name="sender">Window instance</param>
+        /// <param name="e">Event arguments</param>
         public void OnKeyEvent(object sender, Window.KeyEventArgs e)
         {
             if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
