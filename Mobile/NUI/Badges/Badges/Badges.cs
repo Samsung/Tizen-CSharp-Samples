5f5f5cb9cc9f403df42bf66b7707092cf5eccda6
b.chrescionk <b.chrescionk@samsung.com>
2022-01-14 17:30:27 +0100

    [NUI] Updateing comments in Badges and Location samples

--- Mobile/NUI/Badges/Badges/Badges.cs
+++ Mobile/NUI/Badges/Badges/Badges.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2017 Samsung Electronics Co., Ltd. All rights reserved.
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
